f071cf5837f3dbe4d0722d715d5e5ead5e75677e
Diego <diegosantos4957@gmail.com>
2025-08-22 19:19:31 -0300

    feat: create User model class with id, name, and email fields

--- src/main/java/com/diego/list/customers/controller/UsersController.java
+++ src/main/java/com/diego/list/customers/controller/UsersController.java
@@ -1,12 +1,20 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UsersController {
     @GetMapping("/users")
     public String Users() {
+        var users = List.of(
+                new User(1, "Diego", ""),
+                new User(1, "Diego", ""),
+                new User(1, "Diego", "")
+        );
         return "Users endpoint is working!";
     }
 }
