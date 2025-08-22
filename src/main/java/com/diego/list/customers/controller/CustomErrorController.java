fa443c11a7c7569ccdb7c4f1c1a9f7c8b80b535f
Diego <diegosantos4957@gmail.com>
2025-08-22 19:15:28 -0300

    feat: add CustomErrorController for handling custom error responses

--- src/main/java/com/diego/list/customers/controller/CustomErrorController.java
+++ src/main/java/com/diego/list/customers/controller/CustomErrorController.java
@@ -1,4 +1,16 @@
 package com.diego.list.customers.controller;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
 public class CustomErrorController {
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> handle404(NoHandlerFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Página não encontrada (404)");
+    }
 }
