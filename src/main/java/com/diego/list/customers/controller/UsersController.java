8d4f8eff5dbf9a54628120a1550426afbcd0cf82
Diego <diegosantos4957@gmail.com>
2025-08-23 23:51:21 -0300

    feat: initialize UsersServices in UsersController and add logging

--- src/main/java/com/diego/list/customers/controller/UsersController.java
+++ src/main/java/com/diego/list/customers/controller/UsersController.java
@@ -1,6 +1,7 @@
 package com.diego.list.customers.controller;
 
 import com.diego.list.customers.model.User;
+import com.diego.list.customers.services.UsersServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,8 @@ import java.util.List;
 public class UsersController {
     @GetMapping("/users")
     public List<User> Users() {
+        UsersServices usersServices = new UsersServices("Diego");
+        System.out.println("UsersController.Users() chamado");
         return List.of(
                 new User(1, "Diego", ""),
                 new User(1, "Diego", ""),
