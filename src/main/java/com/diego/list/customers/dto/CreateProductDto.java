e90e65b02beac3d66e4030fa31fccbafacce64a6
Diego <diegosantos4957@gmail.com>
2025-09-06 00:03:57 -0300

    feat: add validation annotations to CreateProductDto fields

--- src/main/java/com/diego/list/customers/dto/CreateProductDto.java
+++ src/main/java/com/diego/list/customers/dto/CreateProductDto.java
@@ -1,5 +1,7 @@
 package com.diego.list.customers.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +10,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProductDto {
+    @NotBlank(message = "O nome do produto é obrigatório")
     private String name;
+
+    @NotNull(message = "O preço do produto é obrigatório")
     private Integer price;
+
+    @NotNull(message = "A quantidade em estoque é obrigatória")
     private Integer quantity_in_stock;
+
+
     private String description;
+
+    @NotNull(message = "A categoria do produto é obrigatória")
     private String category;
 }
