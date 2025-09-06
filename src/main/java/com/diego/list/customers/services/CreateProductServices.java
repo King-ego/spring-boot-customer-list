9391d17b9691dcb56feb495c828e8fb3c4ac5336
Diego <diegosantos4957@gmail.com>
2025-09-06 20:25:09 -0300

    feat: add CreateProductServices class for product creation functionality

---
+++ src/main/java/com/diego/list/customers/services/CreateProductServices.java
@@ -0,0 +1,10 @@
+package com.diego.list.customers.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateProductServices {
+    public CreateProductServices(){
+        System.out.println("Service de criação de produto iniciado");
+    }
+}
