7a7877e1991238c9883102360cd2e5038cd131ac
Diego <diegosantos4957@gmail.com>
2025-09-05 23:54:07 -0300

    feat: implement ProductController with RESTful endpoints for product management

--- src/main/java/com/diego/list/customers/controller/ProductController.java
+++ src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -2,9 +2,13 @@ package com.diego.list.customers.controller;
 
 import com.diego.list.customers.dto.CreateProductDto;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
-    @PostMapping("/products")
+    @PostMapping
     public String createProduct(CreateProductDto product) {
         System.out.println(product);
         return "Produto criado com sucesso!";
