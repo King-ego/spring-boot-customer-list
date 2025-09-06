d7937f57a9c26962576abeef6e5eb3ad61f527ee
Diego <diegosantos4957@gmail.com>
2025-09-06 00:13:47 -0300

    feat: add exception handler for MethodArgumentNotValidException in GlobalExceptionHandler

--- src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
@@ -1,7 +1,15 @@
 package com.diego.list.customers.controller.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+    }
 }
