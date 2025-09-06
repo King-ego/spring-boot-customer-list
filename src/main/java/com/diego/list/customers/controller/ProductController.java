db94c16d1ed5b56f8b89ffa808d4bf170e676f51
Diego <diegosantos4957@gmail.com>
2025-09-06 00:04:10 -0300

    feat: add validation to createProduct method in ProductController

--- src/main/java/com/diego/list/customers/controller/ProductController.java
+++ src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.diego.list.customers.controller;
 
 import com.diego.list.customers.dto.CreateProductDto;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductController {
     @PostMapping
-    public String createProduct(CreateProductDto product) {
+    public String createProduct(@Valid @RequestBody CreateProductDto product) {
         System.out.println(product);
         return "Produto criado com sucesso!";
     }
