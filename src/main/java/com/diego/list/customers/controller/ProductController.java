9b8b37fb6406bc6a65d693dc009c847631479f74
Diego <diegosantos4957@gmail.com>
2025-09-05 23:24:56 -0300

    feat: update createProduct endpoint to accept CreateProductDto

--- src/main/java/com/diego/list/customers/controller/ProductController.java
+++ src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.dto.CreateProductDto;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public class ProductController {
     @PostMapping("/products")
-    public String createProduct() {
+    public String createProduct(CreateProductDto product) {
+        System.out.println(product);
         return "Produto criado com sucesso!";
     }
 }
