d505fc4a11995ad22b466de7014fc7d01a0141e9
Diego <diegosantos4957@gmail.com>
2025-09-05 23:46:00 -0300

    feat: change Product ID type to UUID and add generated value annotation

--- src/main/java/com/diego/list/customers/model/Product.java
+++ src/main/java/com/diego/list/customers/model/Product.java
@@ -1,10 +1,13 @@
 package com.diego.list.customers.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.UUID;
 
 @Entity
 @Table(name = "products")
@@ -12,7 +15,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
-    private Long id;
+    @Id
+    @GeneratedValue
+    private UUID id;
     private String name;
     private Integer price;
     private Integer quantity_in_stock;
