2679dcbebd869a913f2853d382ee18ad3b0a2d84
Diego <diegosantos4957@gmail.com>
2025-09-06 00:16:14 -0300

    feat: enhance validation exception handling with detailed error response

--- src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.diego.list.customers.controller.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,5 +12,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getFieldErrors().stream()
                 .findFirst()
+                .map(error -> error.getDefaultMessage())
+                .orElse("Erro de validação");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new SimpleError(HttpStatus.BAD_REQUEST.value(), message));
+    }
+
+    static class SimpleError(int status, String message) {
+        public int status;
+        public String message;
     }
 }
