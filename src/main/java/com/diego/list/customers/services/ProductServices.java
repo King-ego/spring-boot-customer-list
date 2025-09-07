304443803482f87ef6a52291583346ad7791cdeb
Diego <diegosantos4957@gmail.com>
2025-09-06 21:47:40 -0300

    feat: add CreateProductServices class for product creation functionality

--- src/main/java/com/diego/list/customers/services/ProductServices.java
+++ src/main/java/com/diego/list/customers/services/ProductServices.java
@@ -1,6 +1,7 @@
 package com.diego.list.customers.services;
 
 import com.diego.list.customers.command.CreateProductCommand;
+import com.diego.list.customers.model.Product;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,18 @@ public class ProductServices {
         System.out.println("Service de criação de produto iniciado");
     }
 
-    public String createProduct(CreateProductCommand productCommand){
-        return "Produto " + productCommand.getName() + " com preço " + productCommand.getPrice() + " criado com sucesso!";
+    public String createProduct(CreateProductCommand command){
+        Product product = Product.builder()
+                .name(command.getName())
+                .price(command.getPrice())
+                .quantity_in_stock(command.getQuantity_in_stock())
+                .description(command.getDescription())
+                .category(command.getCategory())
+                .build();
+
+        System.out.println("Produto Criado com sucesso" + product);
+
+        return "Produto " + command.getName() + " com preço " + command.getPrice() + " criado com sucesso!";
     }
 
 
