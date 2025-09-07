8345a45716cfc79bbbd2cfab0424d9b338416bd7
Diego <diegosantos4957@gmail.com>
2025-09-07 01:55:35 -0300

    feat: enhance ProductController with logging and command handling for product creation

--- src/main/java/com/diego/list/customers/controller/ProductController.java
+++ src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -1,7 +1,13 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.command.CreateProductCommand;
 import com.diego.list.customers.dto.CreateProductDto;
+import com.diego.list.customers.services.ProductService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
+@Slf4j
 public class ProductController {
+    private final ProductService productService;
+
+
     @PostMapping
     public String createProduct(@Valid @RequestBody CreateProductDto product) {
         System.out.println(product);
-        return "Produto criado com sucesso!";
+
+        CreateProductCommand create = new CreateProductCommand(
+                product.getName(),
+                product.getPrice(),
+                product.getQuantity_in_stock(),
+                product.getDescription(),
+                product.getCategory()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).toString();
     }
 }
