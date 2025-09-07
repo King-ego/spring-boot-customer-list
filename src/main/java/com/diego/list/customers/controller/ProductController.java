7aac322e9115d1a9d4eaac00d888dabcd9f05c9b
Diego <diegosantos4957@gmail.com>
2025-09-07 17:59:17 -0300

    feat: add GET endpoint to retrieve all products in ProductController

--- src/main/java/com/diego/list/customers/controller/ProductController.java
+++ src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -2,16 +2,16 @@ package com.diego.list.customers.controller;
 
 import com.diego.list.customers.command.CreateProductCommand;
 import com.diego.list.customers.dto.CreateProductDto;
+import com.diego.list.customers.model.Product;
 import com.diego.list.customers.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -23,7 +23,6 @@ public class ProductController {
 
     @PostMapping
     public String createProduct(@Valid @RequestBody CreateProductDto product) {
-        System.out.println(product);
 
         CreateProductCommand create = new CreateProductCommand(
                 product.getName(),
@@ -36,5 +35,10 @@ public class ProductController {
         productService.createProduct(create);
 
         return ResponseEntity.status(HttpStatus.CREATED).toString();
+    }
+
+    @GetMapping
+    public List<Product> index(){
+        return productService.getAllProducts();
     }
 }
