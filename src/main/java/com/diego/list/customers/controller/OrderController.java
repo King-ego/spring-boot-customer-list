475b85c0cd590e3e739587fa59f33b45f95db4e8
Diego <diegosantos4957@gmail.com>
2025-09-07 18:25:10 -0300

    feat: enhance OrderController with annotations for order management

--- src/main/java/com/diego/list/customers/controller/OrderController.java
+++ src/main/java/com/diego/list/customers/controller/OrderController.java
@@ -1,4 +1,15 @@
 package com.diego.list.customers.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Controller
+@Slf4j
+@RequestMapping("/orders")
 public class OrderController {
 }
