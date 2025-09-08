6ee0f403b897dfafa353acc6a658416daba874d6
Diego <diegosantos4957@gmail.com>
2025-09-08 07:41:18 -0300

    feat: convert ProductRepository to an interface extending JpaRepository for better data access

--- src/main/java/com/diego/list/customers/repository/ProductRepository.java
+++ src/main/java/com/diego/list/customers/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.diego.list.customers.repository;
 
-public class ProductRepository {
+import com.diego.list.customers.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
