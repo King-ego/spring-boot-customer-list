f0bdb468b2d139732e9b7f402d846550a8afe691
Diego <diegosantos4957@gmail.com>
2025-09-08 07:41:01 -0300

    feat: add POST endpoint to create orders in OrderController

--- src/main/java/com/diego/list/customers/controller/OrderController.java
+++ src/main/java/com/diego/list/customers/controller/OrderController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
@@ -12,4 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @RequestMapping("/orders")
 public class OrderController {
+    @PostMapping
+    public String createOrder(){
+        return "Order created";
+    }
 }
