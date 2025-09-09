f52c95096d05a856ceab0826500f05fd21ae6db8
Diego <diegosantos4957@gmail.com>
2025-09-08 22:24:41 -0300

    feat: add Customer entity and repository for customer management

---
+++ src/main/java/com/diego/list/customers/model/Customer.java
@@ -0,0 +1,30 @@
+package com.diego.list.customers.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "customers")
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+}
