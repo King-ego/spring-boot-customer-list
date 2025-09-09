afd091d431e58f2a4a0306dfbca057ce956ae515
Diego <diegosantos4957@gmail.com>
2025-09-08 22:00:56 -0300

    feat: add createCustomer method to CustomerController for customer creation

--- src/main/java/com/diego/list/customers/controller/CustomerController.java
+++ src/main/java/com/diego/list/customers/controller/CustomerController.java
@@ -1,4 +1,18 @@
 package com.diego.list.customers.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+@Slf4j
 public class CustomerController {
+    @PostMapping
+    public void createCustomer(){
+        log.info("Creating customer...");
+    }
 }
