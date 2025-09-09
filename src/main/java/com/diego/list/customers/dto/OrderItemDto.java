f591ce10662aa1c85d09ca8b27a9cedddb166243
Diego <diegosantos4957@gmail.com>
2025-09-09 00:08:23 -0300

    feat: enhance OrderItemDto with fields for productId and amount

--- src/main/java/com/diego/list/customers/dto/OrderItemDto.java
+++ src/main/java/com/diego/list/customers/dto/OrderItemDto.java
@@ -1,4 +1,13 @@
 package com.diego.list.customers.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDto {
+    private String productId;
+    private Integer amount;
 }
