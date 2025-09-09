9f719eda8b36c85391340a7d6de4179e7f966ab3
Diego <diegosantos4957@gmail.com>
2025-09-09 00:06:59 -0300

    refactor: remove unused RequestBody import in OrderController

--- src/main/java/com/diego/list/customers/controller/OrderController.java
+++ src/main/java/com/diego/list/customers/controller/OrderController.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @AllArgsConstructor
