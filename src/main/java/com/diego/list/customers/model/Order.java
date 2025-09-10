ae5fb8bcb5db0a29ac98f21c8b72739e444fe2e2
Diego <diegosantos4957@gmail.com>
2025-09-09 21:37:50 -0300

    feat: add Order entity for managing order data

---
+++ src/main/java/com/diego/list/customers/model/Order.java
@@ -0,0 +1,32 @@
+package com.diego.list.customers.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private UUID customerId;
+
+    private UUID productId;
+
+    private Integer quantity;
+
+    private String status;
+}
