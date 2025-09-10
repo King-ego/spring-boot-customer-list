70f051ab007ce75c9fe162061c5fc74273b872ad
Diego <diegosantos4957@gmail.com>
2025-09-10 08:39:54 -0300

    feat: implement CreateOrder method in OrderService with logging

--- src/main/java/com/diego/list/customers/services/OrderService.java
+++ src/main/java/com/diego/list/customers/services/OrderService.java
@@ -1,7 +1,21 @@
 package com.diego.list.customers.services;
 
+import com.diego.list.customers.command.CreateOrderCommand;
+import com.diego.list.customers.model.Order;
+import com.diego.list.customers.repository.OrderRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
+@Transactional
 @Service
+@AllArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    public void CreateOrder(CreateOrderCommand order) {
+        log.info("Creando orden");
+    }
 }
