d89abe50e7f9daf86845fee759c5f8b4f4e1f7ce
Diego <diegosantos4957@gmail.com>
2025-09-09 22:01:06 -0300

    feat: add Lombok annotations to CreateOrderCommand for boilerplate reduction

--- src/main/java/com/diego/list/customers/command/CreateOrderCommand.java
+++ src/main/java/com/diego/list/customers/command/CreateOrderCommand.java
@@ -1,7 +1,16 @@
 package com.diego.list.customers.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CreateOrderCommand {
     private UUID customerId;
 
