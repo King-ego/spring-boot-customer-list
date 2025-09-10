94ee0c34eadb4ce440be49858651c2c73e2cd36f
Diego <diegosantos4957@gmail.com>
2025-09-09 22:26:34 -0300

    feat: change customerId type in CreateOrderDto from String to UUID

--- src/main/java/com/diego/list/customers/dto/CreateOrderDto.java
+++ src/main/java/com/diego/list/customers/dto/CreateOrderDto.java
@@ -6,13 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateOrderDto {
     @NotNull(message = "O Id do cliente é obrigatório")
-    private String customerId;
+    private UUID customerId;
 
     @NotNull(message = "A lista de itens do pedido é obrigatória")
     private List<OrderItemDto> order;
