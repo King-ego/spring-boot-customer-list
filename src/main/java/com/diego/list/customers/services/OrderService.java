3602bda8f8a25f20928b68266eed3a1ed40e6258
Diego <diegosantos4957@gmail.com>
2025-09-10 08:48:00 -0300

    feat: add OrderItemCommand import and initialize items list in CreateOrder method

--- src/main/java/com/diego/list/customers/services/OrderService.java
+++ src/main/java/com/diego/list/customers/services/OrderService.java
@@ -1,11 +1,14 @@
 package com.diego.list.customers.services;
 
 import com.diego.list.customers.command.CreateOrderCommand;
+import com.diego.list.customers.command.OrderItemCommand;
 import com.diego.list.customers.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Transactional
@@ -15,6 +18,9 @@ public class OrderService {
     private final OrderRepository orderRepository;
 
     public void CreateOrder(CreateOrderCommand order) {
+        List<OrderItemCommand> items = order.getOrders();
+
+
         log.info("Creando orden");
     }
 }
