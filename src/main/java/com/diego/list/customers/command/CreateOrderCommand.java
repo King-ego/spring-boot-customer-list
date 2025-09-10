9053c1612137a8885fdd1bd752fabd1166be8b5a
Diego <diegosantos4957@gmail.com>
2025-09-09 21:54:48 -0300

    feat: add CreateOrderCommand class for order creation

---
+++ src/main/java/com/diego/list/customers/command/CreateOrderCommand.java
@@ -0,0 +1,11 @@
+package com.diego.list.customers.command;
+
+import java.util.UUID;
+
+public class CreateOrderCommand {
+    private UUID customerId;
+
+    private UUID productId;
+
+    private Integer quantity;
+}
