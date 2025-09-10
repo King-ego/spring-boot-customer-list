86ef82bc8b847b25a285ffb8dedf0d67d78c51c4
Diego <diegosantos4957@gmail.com>
2025-09-09 22:59:50 -0300

    feat: refactor OrderController to use CreateOrderDto and handle order creation

--- src/main/java/com/diego/list/customers/controller/OrderController.java
+++ src/main/java/com/diego/list/customers/controller/OrderController.java
@@ -1,20 +1,33 @@
 package com.diego.list.customers.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.diego.list.customers.command.CreateOrderCommand;
+import com.diego.list.customers.command.OrderItemCommand;
+import com.diego.list.customers.dto.CreateOrderDto;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Controller
+import java.util.stream.Collectors;
+
+
+@RestController
 @Slf4j
 @RequestMapping("/orders")
 public class OrderController {
     @PostMapping
-    public String createOrder(){
-        return "Order created";
+    public void createOrder(@Valid @RequestBody CreateOrderDto orderDto) {
+        log.info("Creating order {}", orderDto);
+
+        CreateOrderCommand order = new CreateOrderCommand(
+                orderDto.getCustomerId(),
+                orderDto.getOrders().stream()
+                        .map(item -> new OrderItemCommand(item.getProductId(), item.getAmount()))
+                        .collect(Collectors.toList())
+        );
+
+        log.info("Creating order {}", order);
     }
 }
