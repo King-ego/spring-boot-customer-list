d5f1f06f474f4446e153b5007f2327b4bd986bbd
Diego <diegosantos4957@gmail.com>
2025-09-11 01:13:58 -0300

    feat: add custom exception handler for CustomException

--- src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diego/list/customers/controller/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.diego.list.customers.controller.exception;
 
+import com.diego.list.customers.errors.CustomException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,13 +14,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getFieldErrors().stream()
                 .findFirst()
-                .map(error -> error.getDefaultMessage())
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .orElse("Erro de validação");
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new SimpleError(HttpStatus.BAD_REQUEST.value(), message));
     }
 
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handleCustomException(CustomException ex) {
+        return ResponseEntity
+                .status(ex.getHttpStatus())
+                .body(new SimpleError(ex.getHttpStatus().value(), ex.getMessage()));
+    }
 
     record SimpleError(int status, String message) {
 
