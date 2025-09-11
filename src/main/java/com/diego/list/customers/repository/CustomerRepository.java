68fe24f5667320ed46d3ea7e7eb850e736272ec4
Diego <diegosantos4957@gmail.com>
2025-09-10 23:46:15 -0300

    feat: implement CustomerRepository interface for customer data access

--- src/main/java/com/diego/list/customers/repository/CustomerRepository.java
+++ src/main/java/com/diego/list/customers/repository/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.diego.list.customers.repository;
 
-public class CustomerRepository {
-}
+import com.diego.list.customers.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> { }
