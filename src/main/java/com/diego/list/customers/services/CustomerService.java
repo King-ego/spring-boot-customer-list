7c2b1ff54e1cb86d6bd80f3c43b656af3f9fb989
Diego <diegosantos4957@gmail.com>
2025-09-10 23:43:59 -0300

    feat: add CustomerService to handle customer creation

---
+++ src/main/java/com/diego/list/customers/services/CustomerService.java
@@ -0,0 +1,28 @@
+package com.diego.list.customers.services;
+
+import com.diego.list.customers.command.CreateCustomerCommand;
+import com.diego.list.customers.model.Customer;
+import com.diego.list.customers.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Transactional
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public void create(CreateCustomerCommand command){
+        Customer customer = Customer.builder()
+                .name(command.getName())
+                .email(command.getEmail())
+                .phone(command.getPhone())
+                .address(command.getAddress())
+                .build();
+
+        customerRepository.save(customer);
+    }
+}
