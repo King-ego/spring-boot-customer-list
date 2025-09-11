39bbdbead8bac9ab99ba659c75331efbe31cc385
Diego <diegosantos4957@gmail.com>
2025-09-11 00:02:53 -0300

    feat: add method to find customer by email in CustomerRepository

--- src/main/java/com/diego/list/customers/repository/CustomerRepository.java
+++ src/main/java/com/diego/list/customers/repository/CustomerRepository.java
@@ -2,7 +2,14 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> { }
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query("SELECT c FROM customers c WHERE c.email = :email")
+    Optional<Customer> findByEmail(String email);
+}
