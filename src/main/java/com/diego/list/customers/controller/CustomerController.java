a1cd3ba842cfcd4c8a56786776f0ca240108e0dc
Diego <diegosantos4957@gmail.com>
2025-09-11 22:44:32 -0300

    feat: integrate CustomerService for customer creation in CustomerController

--- src/main/java/com/diego/list/customers/controller/CustomerController.java
+++ src/main/java/com/diego/list/customers/controller/CustomerController.java
@@ -1,6 +1,8 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.command.CreateCustomerCommand;
 import com.diego.list.customers.dto.CreateCustomersDto;
+import com.diego.list.customers.services.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerController {
+
+    private final CustomerService customerService;
+
     @PostMapping
     public void createCustomer(@Valid @RequestBody CreateCustomersDto createCustomersDto){
-        log.info("Creating customer...");
+        CreateCustomerCommand customer = new CreateCustomerCommand(
+                createCustomersDto.getName(),
+                createCustomersDto.getEmail(),
+                createCustomersDto.getPhone(),
+                createCustomersDto.getAddress()
+        );
+
+        customerService.create(customer);
     }
 }
