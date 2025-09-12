5b1d7c5499e067333882259fe0437dd3a31209f9
Diego <diegosantos4957@gmail.com>
2025-09-11 22:44:05 -0300

    feat: add validation fields for customer details in CreateCustomersDto

--- src/main/java/com/diego/list/customers/dto/CreateCustomersDto.java
+++ src/main/java/com/diego/list/customers/dto/CreateCustomersDto.java
@@ -1,4 +1,24 @@
 package com.diego.list.customers.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCustomersDto {
+    @NotBlank(message = "O nome do cliente é obrigatório")
+    private String name;
+
+    @NotNull(message = "O Email do cliente é obrigatória")
+    private String email;
+
+    @NotNull(message = "O Endereço do cliente é obrigatória")
+    private String address;
+
+    @NotNull(message = "O Telefone do cliente é obrigatória")
+    private String phone;
 }
