5d166724ccc2255ce32a971648231939a277dd81
Diego <diegosantos4957@gmail.com>
2025-09-12 16:11:42 -0300

    feat: add findByName method to ProductRepository

--- src/main/java/com/diego/list/customers/repository/ProductRepository.java
+++ src/main/java/com/diego/list/customers/repository/ProductRepository.java
@@ -2,10 +2,14 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
+    Optional<Product> findByName(String name);
 }
