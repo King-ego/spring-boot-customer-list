f0a6ee9ba4400a94dded01aaf69d869c96105335
Diego <diegosantos4957@gmail.com>
2025-09-12 16:13:57 -0300

    feat: add UUID productId field to OrderItemCommand and implement Lombok annotations

--- src/main/java/com/diego/list/customers/command/OrderItemCommand.java
+++ src/main/java/com/diego/list/customers/command/OrderItemCommand.java
@@ -1,4 +1,18 @@
 package com.diego.list.customers.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemCommand {
+    private UUID productId;
+
+    private Integer amount;
 }
