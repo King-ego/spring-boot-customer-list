83479156e898139c3c258f89077e2cbe98ffd5ba
Diego <diegosantos4957@gmail.com>
2025-09-11 22:43:59 -0300

    feat: add fields for customer details in CreateCustomerCommand

--- src/main/java/com/diego/list/customers/command/CreateCustomerCommand.java
+++ src/main/java/com/diego/list/customers/command/CreateCustomerCommand.java
@@ -1,4 +1,20 @@
 package com.diego.list.customers.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCustomerCommand {
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private String address;
 }
