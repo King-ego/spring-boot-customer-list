bae95295ccf503ef441d18417f23478f64663a2c
Diego <diegosantos4957@gmail.com>
2025-09-12 16:11:35 -0300

    feat: change identifier type from Long to UUID in UserRepository

--- src/main/java/com/diego/list/customers/repository/UserRepository.java
+++ src/main/java/com/diego/list/customers/repository/UserRepository.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 
     List<User> findByNameContainingIgnoreCase(String name);
 
