ef9f7a3a2f3756bad9fc7bc67d0378d0006bf8d6
Diego <diegosantos4957@gmail.com>
2025-09-13 01:55:26 -0300

    feat: change user id field from Long to UUID for improved uniqueness

--- src/main/java/com/diego/list/customers/model/User.java
+++ src/main/java/com/diego/list/customers/model/User.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "users")
 @Data
@@ -13,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private UUID id;
     private String name;
     private String email;
 }
