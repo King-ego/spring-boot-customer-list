1c572b9381787ccb042a5ca56d75f2694a2537d0
Diego <diegosantos4957@gmail.com>
2025-09-13 01:55:07 -0300

    feat: update OrderItemDto to use UUID for productId and add validation constraints

--- src/main/java/com/diego/list/customers/dto/OrderItemDto.java
+++ src/main/java/com/diego/list/customers/dto/OrderItemDto.java
@@ -1,13 +1,19 @@
 package com.diego.list.customers.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItemDto {
-    private String productId;
+    @NotNull(message = "O Id do produto é obrigatório")
+    private UUID productId;
+
+    @NotNull(message = "A quantidade do produto é obrigatória")
     private Integer amount;
 }
