462cb89eed65328488f1c13df30449269488e6be
Diego <diegosantos4957@gmail.com>
2025-09-13 01:55:13 -0300

    feat: add customer existence check in createOrder method and throw CustomException if not found

--- src/main/java/com/diego/list/customers/services/OrderService.java
+++ src/main/java/com/diego/list/customers/services/OrderService.java
@@ -2,15 +2,19 @@ package com.diego.list.customers.services;
 
 import com.diego.list.customers.command.CreateOrderCommand;
 import com.diego.list.customers.command.OrderItemCommand;
+import com.diego.list.customers.errors.CustomException;
+import com.diego.list.customers.model.Customer;
 import com.diego.list.customers.model.Order;
 import com.diego.list.customers.repository.CustomerRepository;
 import com.diego.list.customers.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
@@ -24,6 +28,12 @@ public class OrderService {
     public void createOrder(CreateOrderCommand command) {
         List<OrderItemCommand> items = command.getOrders();
         UUID orderId = UUID.randomUUID();
+
+        Optional<Customer> customer = customerRepository.findById(command.getCustomerId());
+
+        if (customer.isPresent()) {
+            throw new CustomException("Customer not found", HttpStatus.NOT_FOUND);
+        }
 
         items.forEach(item -> {
             Order order = Order.builder()
