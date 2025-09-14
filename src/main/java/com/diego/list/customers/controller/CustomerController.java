68390a44be25047f49422be700e17ef7fa0b03e2
Diego <diegosantos4957@gmail.com>
2025-09-13 21:12:04 -0300

    feat: rename Customers method to CustomerById and add id parameter for improved functionality

--- src/main/java/com/diego/list/customers/controller/CustomerController.java
+++ src/main/java/com/diego/list/customers/controller/CustomerController.java
@@ -6,6 +6,7 @@ import com.diego.list.customers.services.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -29,7 +30,7 @@ public class CustomerController {
     }
 
     @GetMapping
-    public String Customers(){
+    public String CustomerById(@Param("id") String id){
         return "Hello World";
     }
 }
