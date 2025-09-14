860ff473a7570513cc54d40a5b2034951d240a99
Diego <diegosantos4957@gmail.com>
2025-09-14 13:55:37 -0300

    feat: implement RabbitMQConsumer with message receiving and logging

--- src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConsumer.java
+++ src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConsumer.java
@@ -1,4 +1,14 @@
 package com.diego.list.customers.fila.exchange;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class RabbitMQConsumer {
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
+    public void receiveMessage(String message) {
+        log.info("Received message from exchange: {}", message);
+    }
 }
