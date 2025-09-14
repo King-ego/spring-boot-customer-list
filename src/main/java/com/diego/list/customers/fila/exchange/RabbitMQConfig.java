754ae5196ffc0d3f1aef207023e12cbdb0775336
Diego <diegosantos4957@gmail.com>
2025-09-14 13:45:50 -0300

    feat: add RabbitMQConfig class for RabbitMQ configuration setup

--- src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
+++ src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
@@ -1,4 +1,32 @@
 package com.diego.list.customers.fila.exchange;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Queue;
+
+@Configuration
 public class RabbitMQConfig {
+    public static final String QUEUE_NAME = "customer-queue";
+    public static final String EXCHANGE_NAME = "customer-exchange";
+    public static final String ROUTING_KEY = "customer-routing-key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
 }
