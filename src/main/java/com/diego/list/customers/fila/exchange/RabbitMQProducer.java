ddf63abc973e5b0c0e0da88fb4de435adfa5e7ae
Diego <diegosantos4957@gmail.com>
2025-09-14 13:53:03 -0300

    feat: enhance RabbitMQProducer with message sending functionality and logging

--- src/main/java/com/diego/list/customers/fila/exchange/RabbitMQProducer.java
+++ src/main/java/com/diego/list/customers/fila/exchange/RabbitMQProducer.java
@@ -1,7 +1,20 @@
 package com.diego.list.customers.fila.exchange;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class RabbitMQProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, message);
+        log.info("Sent message to exchange: {}", RabbitMQConfig.EXCHANGE_NAME);
+    }
 }
