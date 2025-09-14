3591831307c9fac3303658301016ad34aa6acfff
Diego <diegosantos4957@gmail.com>
2025-09-14 15:18:11 -0300

    refactor: remove unused import for Param in CustomerController

--- src/main/java/com/diego/list/customers/controller/CustomerController.java
+++ src/main/java/com/diego/list/customers/controller/CustomerController.java
@@ -7,7 +7,6 @@ import com.diego.list.customers.services.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
