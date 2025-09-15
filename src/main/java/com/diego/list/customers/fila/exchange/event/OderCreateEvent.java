03d2762e7e8a8818f97124a2a0817ec9d4fd6da4
Diego <diegosantos4957@gmail.com>
2025-09-14 21:09:07 -0300

    feat: enhance OderCreateEvent class with order details and Lombok annotations

--- src/main/java/com/diego/list/customers/fila/exchange/event/OderCreateEvent.java
+++ src/main/java/com/diego/list/customers/fila/exchange/event/OderCreateEvent.java
@@ -1,4 +1,17 @@
 package com.diego.list.customers.fila.exchange.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OderCreateEvent {
+    private UUID orderId;
+    private UUID customerId;
+    private String orderStatus;
+    private Float orderTotal;
 }
