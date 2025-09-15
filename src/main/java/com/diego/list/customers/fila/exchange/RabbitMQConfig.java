40ce602b55fa3ecce8edd8798cb80cb3617fc142
Diego <diegosantos4957@gmail.com>
2025-09-15 09:46:31 -0300

    feat: add RabbitTemplate and Jackson2JsonMessageConverter to RabbitMQ configuration

--- src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
+++ src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package com.diego.list.customers.fila.exchange;
 
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +28,17 @@ public class RabbitMQConfig {
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
     }
 }
