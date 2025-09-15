8fef38a23300f7418d76e39d0005cce34560deed
Diego <diegosantos4957@gmail.com>
2025-09-14 23:25:27 -0300

    fix: change orderTotal type from Float to BigDecimal for improved precision

--- src/main/java/com/diego/list/customers/fila/exchange/event/OderCreateEvent.java
+++ src/main/java/com/diego/list/customers/fila/exchange/event/OderCreateEvent.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
@@ -13,5 +14,5 @@ public class OderCreateEvent {
     private UUID orderId;
     private UUID customerId;
     private String orderStatus;
-    private Float orderTotal;
+    private BigDecimal orderTotal;
 }
