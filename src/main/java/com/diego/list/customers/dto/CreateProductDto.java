f89707cc1b70357b929075f19790e9e2785a67c2
Diego <diegosantos4957@gmail.com>
2025-09-15 20:53:42 -0300

    feat: change price type from Integer to BigDecimal in CreateProductDto

--- src/main/java/com/diego/list/customers/dto/CreateProductDto.java
+++ src/main/java/com/diego/list/customers/dto/CreateProductDto.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,7 +16,7 @@ public class CreateProductDto {
     private String name;
 
     @NotNull(message = "O preço do produto é obrigatório")
-    private Integer price;
+    private BigDecimal price;
 
     @NotNull(message = "A quantidade em estoque é obrigatória")
     private Integer quantity_in_stock;
