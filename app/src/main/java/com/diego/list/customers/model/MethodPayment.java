2b9ea61d5487dd3261945dcf33f1e906b094b5ae
Diego <diegosantos4957@gmail.com>
2025-09-20 19:01:06 -0300

    feat: add MethodPayment entity with fields for payment methods

--- app/src/main/java/com/diego/list/customers/model/MethodPayment.java
+++ app/src/main/java/com/diego/list/customers/model/MethodPayment.java
@@ -1,4 +1,23 @@
 package com.diego.list.customers.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "method_payments")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MethodPayment {
+    private UUID id;
+    private UUID customerId;
+    private String type;
+    private String token_method_payment;
+    private boolean isDefault;
 }
