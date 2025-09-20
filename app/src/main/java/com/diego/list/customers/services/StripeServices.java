f943bb8b853ebfc31e4a9b1ec70d0876b558f163
Diego <diegosantos4957@gmail.com>
2025-09-20 11:51:24 -0300

    feat: enhance StripeServices with logging and transaction management

--- app/src/main/java/com/diego/list/customers/services/StripeServices.java
+++ app/src/main/java/com/diego/list/customers/services/StripeServices.java
@@ -1,4 +1,18 @@
 package com.diego.list.customers.services;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Transactional
+@AllArgsConstructor
 public class StripeServices {
+    private final String stripeApiKey  = System.getenv("STRIPE_API_KEY");
+
+    public void createCustomer() {
+        log.info("Creating Stripe Customer {}", stripeApiKey);
+    }
 }
