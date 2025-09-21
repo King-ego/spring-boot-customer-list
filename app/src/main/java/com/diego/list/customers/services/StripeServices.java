5971c017ee4582700f099fd5c2ecf9acaf2382a5
Diego <diegosantos4957@gmail.com>
2025-09-21 00:04:56 -0300

    feat: enhance customer creation in Stripe by adding error handling

--- app/src/main/java/com/diego/list/customers/services/StripeServices.java
+++ app/src/main/java/com/diego/list/customers/services/StripeServices.java
@@ -1,10 +1,12 @@
 package com.diego.list.customers.services;
 
+import com.diego.list.customers.errors.CustomException;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.stripe.model.Customer;
 
@@ -18,7 +20,7 @@ import java.util.Map;
 public class StripeServices {
     private final String stripeApiKey  = System.getenv("STRIPE_API_KEY");
 
-    public String createCustomer(String email, String name) throws StripeException {
+    public String createCustomer(String email, String name) {
         Stripe.apiKey = stripeApiKey;
 
         Map<String, Object> params = new HashMap<>();
@@ -26,10 +28,12 @@ public class StripeServices {
         params.put("email", email);
         params.put("name", name);
 
-        Customer customer = Customer.create(params);
-
-        log.info("Customer created in Stripe");
-
-        return customer.getId();
+        try {
+            Customer customer = Customer.create(params);
+            return customer.getId();
+        }catch (StripeException e){
+            log.error("Error creating customer in Stripe: {}", e.getMessage());
+            throw new CustomException("Error creating customer in Stripe", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
