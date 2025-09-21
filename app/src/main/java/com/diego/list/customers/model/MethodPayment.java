c14e3731fee83fd3d4b241273eaa7f32bb1bcec1
Diego <diegosantos4957@gmail.com>
2025-09-21 00:04:44 -0300

    feat: update MethodPayment entity to include additional payment method fields and replace customer relationship with customerId

--- app/src/main/java/com/diego/list/customers/model/MethodPayment.java
+++ app/src/main/java/com/diego/list/customers/model/MethodPayment.java
@@ -1,10 +1,8 @@
 package com.diego.list.customers.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,18 +13,27 @@ import java.util.UUID;
 @Table(name = "method_payments")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class MethodPayment {
+    @Id
+    @GeneratedValue
     private UUID id;
-
-    @ManyToOne
-    @JoinColumn(name = "customer_id", nullable = false)
-    private Customer customer;
 
     private UUID customerId;
 
     private String type;
 
-    private String token_method_payment;
+    private String method_payment_id;
 
     private boolean isDefault;
+
+    private String brand;
+
+    private String exp_month;
+
+    private String exp_year;
+
+    private String last4;
+
+    private String funding;
 }
