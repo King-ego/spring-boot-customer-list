c97bf4cf46d4efc3dd05a06b264e5636768adaf5
Diego <diegosantos4957@gmail.com>
2025-09-21 23:06:25 -0300

    feat: add request mapping for MethodPaymentController

--- app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
+++ app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
@@ -1,4 +1,7 @@
 package com.diego.list.customers.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/method-payments")
 public class MethodPaymentController {
 }
