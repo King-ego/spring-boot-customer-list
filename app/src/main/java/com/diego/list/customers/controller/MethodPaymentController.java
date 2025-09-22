f65a48bdd0edf8f9f3ce6cb2bdf108e5cc616df2
Diego <diegosantos4957@gmail.com>
2025-09-21 23:07:07 -0300

    feat: add @Slf4j annotation for logging in MethodPaymentController

--- app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
+++ app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
@@ -1,9 +1,11 @@
 package com.diego.list.customers.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 @RequestMapping("/method-payments")
 public class MethodPaymentController {
 }
