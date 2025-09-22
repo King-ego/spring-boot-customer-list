62dfafcd34db7ca9b479d8b7a038b55f17492a49
Diego <diegosantos4957@gmail.com>
2025-09-21 23:06:39 -0300

    feat: add @Controller annotation to MethodPaymentController

--- app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
+++ app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
@@ -1,7 +1,9 @@
 package com.diego.list.customers.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/method-payments")
 public class MethodPaymentController {
 }
