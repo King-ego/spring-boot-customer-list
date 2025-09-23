1305cd6b978b79cfb536febec3ca455e51df7751
Diego <diegosantos4957@gmail.com>
2025-09-22 21:29:58 -0300

    feat: add @NoArgsConstructor annotation to MethodPaymentService

--- app/src/main/java/com/diego/list/customers/services/MethodPaymentService.java
+++ app/src/main/java/com/diego/list/customers/services/MethodPaymentService.java
@@ -1,11 +1,13 @@
 package com.diego.list.customers.services;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @AllArgsConstructor
+@NoArgsConstructor
 public class MethodPaymentService {
 }
