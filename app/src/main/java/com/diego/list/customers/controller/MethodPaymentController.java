5e462131f45e055260f1a8ca15c17f7a0dae2cf1
Diego <diegosantos4957@gmail.com>
2025-09-24 22:06:40 -0300

    feat: update createMethodPayment to accept CreateMethodPaymentDto

--- app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
+++ app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
@@ -1,10 +1,12 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.dto.CreateMethodPaymentDto;
 import com.diego.list.customers.services.MethodPaymentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,7 +18,7 @@ public class MethodPaymentController {
     private final MethodPaymentService methodPaymentService;
 
     @PostMapping
-    public void createMethodPayment() {
+    public void createMethodPayment(@RequestBody CreateMethodPaymentDto methodPaymentDto) {
         log.info("Creating method payment");
         methodPaymentService.createMethodPayment();
     }
