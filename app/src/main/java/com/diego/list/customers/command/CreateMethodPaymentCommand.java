6fed49a98444a7abb59267cca0a6c714abb74be5
Diego <diegosantos4957@gmail.com>
2025-09-24 22:12:08 -0300

    feat: add fields to CreateMethodPaymentCommand for payment method details

--- app/src/main/java/com/diego/list/customers/command/CreateMethodPaymentCommand.java
+++ app/src/main/java/com/diego/list/customers/command/CreateMethodPaymentCommand.java
@@ -5,9 +5,28 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateMethodPaymentCommand {
+    private UUID customerId;
+
+    private String type;
+
+    private String method_payment_id;
+
+    private boolean isDefault;
+
+    private String brand;
+
+    private String exp_month;
+
+    private String exp_year;
+
+    private String last4;
+
+    private String funding;
 }
