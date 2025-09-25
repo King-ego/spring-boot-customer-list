6e696db274c856608818874f9d55b27932fca43c
Diego <diegosantos4957@gmail.com>
2025-09-24 22:15:34 -0300

    feat: initialize CreateMethodPaymentCommand in createMethodPayment method

--- app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
+++ app/src/main/java/com/diego/list/customers/controller/MethodPaymentController.java
@@ -1,5 +1,6 @@
 package com.diego.list.customers.controller;
 
+import com.diego.list.customers.command.CreateMethodPaymentCommand;
 import com.diego.list.customers.dto.CreateMethodPaymentDto;
 import com.diego.list.customers.services.MethodPaymentService;
 import jakarta.validation.Valid;
@@ -21,6 +22,9 @@ public class MethodPaymentController {
     @PostMapping
     public void createMethodPayment(@Valid @RequestBody CreateMethodPaymentDto methodPaymentDto) {
         log.info("Creating method payment");
+        CreateMethodPaymentCommand command = new CreateMethodPaymentCommand(
+
+        );
         methodPaymentService.createMethodPayment();
     }
 }
