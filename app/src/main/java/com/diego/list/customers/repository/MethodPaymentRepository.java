885526f4c07d26fbc3ab3d088908f52876c211b7
Diego <diegosantos4957@gmail.com>
2025-09-25 22:49:23 -0300

    feat: extend MethodPaymentRepository to inherit from JpaRepository

--- app/src/main/java/com/diego/list/customers/repository/MethodPaymentRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/MethodPaymentRepository.java
@@ -1,7 +1,11 @@
 package com.diego.list.customers.repository;
 
+import com.diego.list.customers.model.MethodPayment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface MethodPaymentRepository {
+public interface MethodPaymentRepository extends JpaRepository<MethodPayment, UUID> {
 }
