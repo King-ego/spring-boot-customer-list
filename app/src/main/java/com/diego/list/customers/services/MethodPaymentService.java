ff47232a60514e85b2deeb5cedb2e614a99f38ba
Diego <diegosantos4957@gmail.com>
2025-09-25 22:51:07 -0300

    feat: add MethodPaymentRepository dependency and enable transaction management in MethodPaymentService

--- app/src/main/java/com/diego/list/customers/services/MethodPaymentService.java
+++ app/src/main/java/com/diego/list/customers/services/MethodPaymentService.java
@@ -2,6 +2,8 @@ package com.diego.list.customers.services;
 
 import com.diego.list.customers.command.CreateMethodPaymentCommand;
 import com.diego.list.customers.model.MethodPayment;
+import com.diego.list.customers.repository.MethodPaymentRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,8 +12,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @AllArgsConstructor
-@NoArgsConstructor
+@Transactional
 public class MethodPaymentService {
+    private final MethodPaymentRepository methodPaymentRepository;
     public void createMethodPayment(CreateMethodPaymentCommand command) {
         MethodPayment methodPayment = MethodPayment.builder()
             .customerId(command.getCustomerId())
