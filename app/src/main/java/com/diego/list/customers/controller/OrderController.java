4e4ed90d3a5b8d35fc294670fa90fb5840872df6
Diego <diegosantos4957@gmail.com>
2025-09-27 20:41:38 -0300

    feat: add index endpoint to OrderController

--- app/src/main/java/com/diego/list/customers/controller/OrderController.java
+++ app/src/main/java/com/diego/list/customers/controller/OrderController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 
@@ -38,5 +35,10 @@ public class OrderController {
         orderService.createOrder(order);
 
         log.info("Creating order {}", order);
+    }
+
+    @GetMapping
+    public String index() {
+        return "ok";
     }
 }
