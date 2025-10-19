4d3a31b295452a80cb654cdd918dde0f7bf38224
Diego <diegosantos4957@gmail.com>
2025-10-18 23:38:26 -0300

    feat: update main.go to implement a basic HTTP server using Gin

--- challenge_english/cmd/main.go
+++ challenge_english/cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	if err := server.Run(":8097"); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+}
