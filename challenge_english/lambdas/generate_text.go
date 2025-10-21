13d9c37bea9076a410ccd5d1781ca1ecd6a30e57
Diego <diegosantos4957@gmail.com>
2025-10-21 00:12:13 -0300

    feat: implement RunChallenge struct and associated methods for challenge job execution

--- challenge_english/lambdas/generate_text.go
+++ challenge_english/lambdas/generate_text.go
@@ -2,7 +2,21 @@ package lambdas
 
 import (
 	"github.com/aws/aws-sdk-go-v2/service/lambda"
-	"github.com/aws/aws-sdk-go-v2/service/lambda/types"
-
-	"github.com/aws/aws-sdk-go-v2/config"
 )
+
+type RunChallenge struct {
+	client  *lambda.Client
+	message string
+}
+
+func NewChallengeJob(client *lambda.Client, message string) *RunChallenge {
+	return &RunChallenge{
+		client:  client,
+		message: message,
+	}
+}
+
+func (cj *RunChallenge) RunChallengeJob() {
+	var client = cj.client
+	var message = cj.message
+}
