cd0b38bcb7d5339ca650ccfa9a7a299a43bb745c
Diego <diegosantos4957@gmail.com>
2025-10-22 22:58:10 -0300

    fix: remove unnecessary newline at start of RabbitMQConfig.java

--- app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConfig.java
@@ -1,6 +1,5 @@
 package com.diego.list.customers.fila.exchange;
 
-
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
