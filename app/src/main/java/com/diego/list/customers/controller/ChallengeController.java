04e03e5d402a91b160d9e31de84386dcb6b8a459
Diego <diegosantos4957@gmail.com>
2025-11-02 11:34:41 -0300

    feat: add GET endpoint to ChallengeController

--- app/src/main/java/com/diego/list/customers/controller/ChallengeController.java
+++ app/src/main/java/com/diego/list/customers/controller/ChallengeController.java
@@ -2,6 +2,7 @@ package com.diego.list.customers.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class ChallengeController {
+    @GetMapping
+    public String getChallenge() {
+        return "Challenge endpoint is working!";
+    }
 }
