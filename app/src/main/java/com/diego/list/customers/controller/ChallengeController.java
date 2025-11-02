72644e49c8ecb3899e1a27b9ccf686c015564629
Diego <diegosantos4957@gmail.com>
2025-11-02 11:38:34 -0300

    feat: update GET endpoint in ChallengeController to accept user_id as a path variable

--- app/src/main/java/com/diego/list/customers/controller/ChallengeController.java
+++ app/src/main/java/com/diego/list/customers/controller/ChallengeController.java
@@ -1,18 +1,22 @@
 package com.diego.list.customers.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/challenges")
 @Slf4j
 @RequiredArgsConstructor
 public class ChallengeController {
-    @GetMapping
-    public String getChallenge() {
+    @GetMapping("/:user_id")
+    public String getChallenge(@Valid @PathVariable UUID customer_id) {
         return "Challenge endpoint is working!";
     }
 }
