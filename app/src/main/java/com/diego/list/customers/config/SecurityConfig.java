2d6d383ade4b3858879cdc583d87a87c603f7d9e
Diego <diegosantos4957@gmail.com>
2025-11-13 18:00:21 -0300

    feat: enhance security configuration with method and web security annotations

--- app/src/main/java/com/diego/list/customers/config/SecurityConfig.java
+++ app/src/main/java/com/diego/list/customers/config/SecurityConfig.java
@@ -1,4 +1,11 @@
 package com.diego.list.customers.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 }
