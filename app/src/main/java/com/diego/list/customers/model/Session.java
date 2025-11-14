df29b25709ce68959412f6ee897a47a038d706ba
Diego <diegosantos4957@gmail.com>
2025-11-13 23:31:26 -0300

    feat: enhance Session class with UUID fields and Lombok annotations

--- app/src/main/java/com/diego/list/customers/model/Session.java
+++ app/src/main/java/com/diego/list/customers/model/Session.java
@@ -1,7 +1,25 @@
 package com.diego.list.customers.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
+import java.util.UUID;
+
 @RedisHash("Session")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Session {
+    @Id
+    @GeneratedValue
+    private UUID sessionId;
+
+    private UUID userId;
+
+    private String deviceFingerprint;
+
 }
