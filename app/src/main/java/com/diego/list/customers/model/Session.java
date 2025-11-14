64485312002622e2d2e23febc3dfa434ad2abbcd
Diego <diegosantos4957@gmail.com>
2025-11-13 23:40:07 -0300

    feat: add DeviceInfo class and embed it in Session class

--- app/src/main/java/com/diego/list/customers/model/Session.java
+++ app/src/main/java/com/diego/list/customers/model/Session.java
@@ -1,5 +1,7 @@
 package com.diego.list.customers.model;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -26,4 +28,24 @@ public class Session {
 
     private String userAgent;
 
+    @Embedded
+    private DeviceInfo deviceInfo;
+
+}
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class DeviceInfo {
+    private String browser;
+    private String os;
+    private String screenResolution;
+    private String timezone;
+    private String language;
+    private String country;
+    private String city;
+    private Boolean isMobile;
+    private Boolean isTablet;
+    private Boolean isDesktop;
 }
