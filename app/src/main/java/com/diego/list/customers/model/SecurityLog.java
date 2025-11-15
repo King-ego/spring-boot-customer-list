6fdacd6b6d90d624fb23202206e4dc02da3c0958
Diego <diegosantos4957@gmail.com>
2025-11-14 22:21:33 -0300

    feat: implement SecurityLog entity for logging security events

--- app/src/main/java/com/diego/list/customers/model/SecurityLog.java
+++ app/src/main/java/com/diego/list/customers/model/SecurityLog.java
@@ -1,4 +1,19 @@
 package com.diego.list.customers.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "security_logs")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SecurityLog {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
 }
