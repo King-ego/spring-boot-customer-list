6ccb743cd3538914a69b4ff470df1bf48196f227
Diego <diegosantos4957@gmail.com>
2025-11-14 22:33:34 -0300

    feat: add SessionValidationFilter for validating user sessions

--- app/src/main/java/com/diego/list/customers/filter/SessionValidationFilter.java
+++ app/src/main/java/com/diego/list/customers/filter/SessionValidationFilter.java
@@ -14,6 +14,17 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;*/
 
 import com.diego.list.customers.model.Session;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 @Order(1)
