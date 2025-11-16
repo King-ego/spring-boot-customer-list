212fa7212d4646821b34e670ea3682ab104f930f
Diego <diegosantos4957@gmail.com>
2025-11-16 19:00:14 -0300

    feat: enable JPA and Redis repositories in the application

--- app/src/main/java/com/diego/list/customers/Application.java
+++ app/src/main/java/com/diego/list/customers/Application.java
@@ -3,9 +3,13 @@ package com.diego.list.customers;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @EnableRabbit
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.diego.list.customers.repository")
+@EnableRedisRepositories(basePackages = "com.diego.list.customers.redis")
 public class Application {
 
 	public static void main(String[] args) {
