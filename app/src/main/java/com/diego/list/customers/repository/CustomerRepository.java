0ccac32ecda54db93163aabfb5a592931909defd
Diego <diegosantos4957@gmail.com>
2025-11-17 20:51:27 -0300

    feat: enable JPA repositories for CustomerRepository

--- app/src/main/java/com/diego/list/customers/repository/CustomerRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/CustomerRepository.java
@@ -3,11 +3,13 @@ package com.diego.list.customers.repository;
 import com.diego.list.customers.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@EnableJpaRepositories(basePackageClasses = CustomerRepository.class)
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
