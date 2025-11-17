7c48e3005b8ad7e963ac6f0ebb86054bdcdadeda
Diego <diegosantos4957@gmail.com>
2025-11-17 20:52:15 -0300

    feat: rename SessionRepository to SessionRedisRepository and update userId type to UUID

--- app/src/main/java/com/diego/list/customers/redis/SessionRedisRepository.java
+++ app/src/main/java/com/diego/list/customers/redis/SessionRedisRepository.java
@@ -1,4 +1,4 @@
-package com.diego.list.customers.repository;
+package com.diego.list.customers.redis;
 
 import com.diego.list.customers.model.Session;
 import org.springframework.data.repository.CrudRepository;
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface SessionRepository extends CrudRepository<Session, String> {
+public interface SessionRedisRepository extends CrudRepository<Session, String> {
     Optional<Session> findBySessionId(String sessionId);
-    List<Session> findByUserId(String userId);
+    List<Session> findByUserId(UUID userId);
     List<Session> findByUserIdAndIsActive(String userId, boolean isActive);
     long countByUserIdAndIsActive(String userId, boolean isActive);
 }
