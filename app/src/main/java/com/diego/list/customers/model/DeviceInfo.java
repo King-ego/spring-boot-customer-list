ef0a0d113c0f8558ec2bd9895e289a02e05bd04b
Diego <diegosantos4957@gmail.com>
2025-11-19 21:47:13 -0300

    feat: implement Serializable interface in DeviceInfo and update field types to primitives

--- app/src/main/java/com/diego/list/customers/model/DeviceInfo.java
+++ app/src/main/java/com/diego/list/customers/model/DeviceInfo.java
@@ -4,12 +4,15 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
 
-@Embeddable
+import java.io.Serializable;
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class DeviceInfo {
+public class DeviceInfo implements Serializable {
     private String browser;
     private String os;
     private String screenResolution;
@@ -17,7 +20,7 @@ public class DeviceInfo {
     private String language;
     private String country;
     private String city;
-    private Boolean isMobile;
-    private Boolean isTablet;
-    private Boolean isDesktop;
+    private boolean isMobile;
+    private boolean isTablet;
+    private boolean isDesktop;
 }
