c1a29068b9324aaa4208f460b67805902f26b0b3
Diego <diegosantos4957@gmail.com>
2025-11-22 21:30:12 -0300

    chore: remove unused imports from User.java

--- app/src/main/java/com/diego/list/customers/model/User.java
+++ app/src/main/java/com/diego/list/customers/model/User.java
@@ -1,14 +1,11 @@
 package com.diego.list.customers.model;
 
-import com.stripe.model.issuing.Token;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
