21d3d0d779ee2ae15be26f3466261235ac17da5c
Diego <diegosantos4957@gmail.com>
2025-12-06 20:20:59 -0300

    chore: update request mapping for ProductController to include API prefix

--- app/src/main/java/com/diego/list/customers/controller/ProductController.java
+++ app/src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
