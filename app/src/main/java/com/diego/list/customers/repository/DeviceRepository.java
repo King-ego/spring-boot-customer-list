6ab93c47e64bc5b851dffa59b48946604bc22857
Diego <diegosantos4957@gmail.com>
2025-12-29 21:39:12 -0300

    chore: remove unnecessary import from DeviceRepository.java

--- app/src/main/java/com/diego/list/customers/repository/DeviceRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/DeviceRepository.java
@@ -3,7 +3,6 @@ package com.diego.list.customers.repository;
 import com.diego.list.customers.model.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
