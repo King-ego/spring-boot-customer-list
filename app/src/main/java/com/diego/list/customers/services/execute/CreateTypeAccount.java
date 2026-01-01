7bd61176a8bcac0bc511245002b15bb063508b0e
Diego <diegosantos4957@gmail.com>
2026-01-01 19:25:24 -0300

    refactor: rename createTypeAccount class to CreateTypeAccount and add logging for role creation

---
+++ app/src/main/java/com/diego/list/customers/services/execute/CreateTypeAccount.java
@@ -0,0 +1,14 @@
+package com.diego.list.customers.services.execute;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CreateTypeAccount {
+    public void createRoleCustomer(){
+        log.info("Creating role customer");
+    }
+
+    public void createRoleSeller(){
+        log.info("Updating role seller");
+    }
+}
--- app/src/main/java/com/diego/list/customers/services/execute/createTypeAccount.java
+++
@@ -1,9 +0,0 @@
-package com.diego.list.customers.services.execute;
-
-public class createTypeAccount {
-    public void createRoleCustomer(){
-    }
-
-    public void updateRoleSeller(){
-    }
-}
