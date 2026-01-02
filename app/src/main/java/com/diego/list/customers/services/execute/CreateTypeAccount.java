ad7a0a15c9a680681c104237cae6e6733c0db42a
Diego <diegosantos4957@gmail.com>
2026-01-01 23:24:20 -0300

    feat: implement CreateTypeAccount service for customer and seller role creation

--- app/src/main/java/com/diego/list/customers/services/execute/CreateTypeAccount.java
+++ app/src/main/java/com/diego/list/customers/services/execute/CreateTypeAccount.java
@@ -1,14 +1,32 @@
 package com.diego.list.customers.services.execute;
 
+import com.diego.list.customers.command.createUser.CreateCustomerCommand;
+import com.diego.list.customers.command.createUser.CreateSellerCommand;
+import com.diego.list.customers.model.Customer;
+import com.diego.list.customers.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
+@Service
+@AllArgsConstructor
 public class CreateTypeAccount {
-    public void createRoleCustomer(){
-        log.info("Creating role customer");
+    private final CustomerRepository customerRepository;
+
+    public void createRoleCustomer(CreateCustomerCommand command){
+        Customer customer = Customer.builder()
+                        .userId(command.getUserId())
+                        .document(command.getDocument())
+                        .totalOrders(0)
+                        .totalSpent(0.0)
+                        .build();
+
+        customerRepository.save(customer);
+        log.info("Creating role customer {}", command);
     }
 
-    public void createRoleSeller(){
-        log.info("Updating role seller");
+    public void createRoleSeller(CreateSellerCommand command){
+        log.info("Updating role seller {}", command);
     }
 }
