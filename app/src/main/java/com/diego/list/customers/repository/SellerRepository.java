673a17a1f8db0959c0caeb9da5530ac8109f2895
Diego <diegosantos4957@gmail.com>
2026-01-02 10:36:34 -0300

    feat: add SellerRepository interface for Seller entity management

---
+++ app/src/main/java/com/diego/list/customers/repository/SellerRepository.java
@@ -0,0 +1,11 @@
+package com.diego.list.customers.repository;
+
+import com.diego.list.customers.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SellerRepository extends JpaRepository<Seller, UUID> {
+}
