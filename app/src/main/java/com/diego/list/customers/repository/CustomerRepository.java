3be955e092f7b16040254d3c3e6b95da7a7ec0d9
Diego <diegosantos4957@gmail.com>
2026-01-02 01:48:15 -0300

    feat: update findByUserId query to use User object in CustomerRepository

--- app/src/main/java/com/diego/list/customers/repository/CustomerRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Customer;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,6 +9,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-    @Query("SELECT c FROM Customer c WHERE c.userId = ?1")
-    Optional<Customer> findByUserId(UUID userId);
+    @Query("SELECT c FROM Customer c WHERE c.user.id = :userId")
+    Optional<Customer> findByUserId(@Param("userId") UUID userId);
 }
