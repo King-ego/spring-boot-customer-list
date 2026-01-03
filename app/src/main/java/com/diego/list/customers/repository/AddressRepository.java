6cdbc9b44a2bc3d3294e772fce21afe624dca292
diego <diegouf495@gmail.com>
2026-01-03 15:55:24 -0300

    feat: implement in address repository jpa

--- app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
@@ -1,5 +1,9 @@
 package com.diego.list.customers.repository;
 
+import com.diego.list.customers.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AddressRepository {
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
 }
