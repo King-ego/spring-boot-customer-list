20f9034c7ca9b7db51b2e9b60ea9c13bd7e49465
diego <diegoufob2017@gmail.com>
2026-01-03 15:38:30 -0300

    feat: route create address send

--- app/src/main/java/com/diego/list/customers/controller/AddressController.java
+++ app/src/main/java/com/diego/list/customers/controller/AddressController.java
@@ -1,11 +1,14 @@
 package com.diego.list.customers.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/address")
 @RequiredArgsConstructor
 public class AddressController {
+    @PostMapping("")
+    public void createAddress() {
+
+    }
 }
