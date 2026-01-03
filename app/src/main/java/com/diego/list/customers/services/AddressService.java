eec30a42867b402b1bdff73a7beedc3f896d0d4b
diego <diegouf495@gmail.com>
2026-01-03 16:19:00 -0300

    feat: add repositories in create service address

--- app/src/main/java/com/diego/list/customers/services/AddressService.java
+++ app/src/main/java/com/diego/list/customers/services/AddressService.java
@@ -1,5 +1,8 @@
 package com.diego.list.customers.services;
 
+import com.diego.list.customers.command.CreateAddressCommand;
+import com.diego.list.customers.repository.AddressRepository;
+import com.diego.list.customers.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,7 +13,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @RequiredArgsConstructor
 public class AddressService {
-    public void CreateAddress() {
+    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
+
+    public void CreateAddress(CreateAddressCommand createAddressCommand) {
 
     }
 }
