8cfbde5722099c2f9aaa34b42da8521ece5b4f21
diego <diegoufob2017@gmail.com>
2026-01-03 15:02:16 -0300

    feat: update model address

--- app/src/main/java/com/diego/list/customers/model/Address.java
+++ app/src/main/java/com/diego/list/customers/model/Address.java
@@ -1,4 +1,54 @@
 package com.diego.list.customers.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String recipient;
+
+    private Number zip_code;
+
+    private String street_address;
+
+    private Number street_number;
+
+    private String neighborhood;
+
+    private String city;
+
+    private String state;
+
+    private String complement;
+
+    private String phone_number;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
