62f022d57e7ac727beb86dc09d64a1ba51ce3406
diego <diegouf495@gmail.com>
2026-01-03 23:08:28 -0300

    feat: exist user

--- app/src/main/java/com/diego/list/customers/services/AddressService.java
+++ app/src/main/java/com/diego/list/customers/services/AddressService.java
@@ -1,12 +1,15 @@
 package com.diego.list.customers.services;
 
 import com.diego.list.customers.command.CreateAddressCommand;
+import com.diego.list.customers.model.User;
 import com.diego.list.customers.repository.AddressRepository;
 import com.diego.list.customers.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,6 +20,6 @@ public class AddressService {
     private final AddressRepository addressRepository;
 
     public void CreateAddress(CreateAddressCommand createAddressCommand) {
-
+        Optional<User> existUser = userRepository.findById(createAddressCommand.getUser_id());
     }
 }
