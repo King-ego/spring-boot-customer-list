ddeb44a03faa3974c2dd3473d2f3a70b0d4685cf
diego <diegouf495@gmail.com>
2026-01-04 19:50:40 -0300

    feat: remove api/auth

--- app/src/main/java/com/diego/list/customers/controller/AuthController.java
+++ app/src/main/java/com/diego/list/customers/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.Duration;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping
 @RequiredArgsConstructor
 public class AuthController {
 
