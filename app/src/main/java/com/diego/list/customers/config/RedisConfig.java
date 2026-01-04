7d3052db6b2804a96469a1b2f301f92a180ef218
diego <diegouf495@gmail.com>
2026-01-04 19:52:45 -0300

    feat: log post create redis config init

--- app/src/main/java/com/diego/list/customers/config/RedisConfig.java
+++ app/src/main/java/com/diego/list/customers/config/RedisConfig.java
@@ -1,18 +1,24 @@
 package com.diego.list.customers.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+
 @Configuration
 @EnableRedisRepositories
+@Slf4j
 public class RedisConfig {
     @Value("${spring.redis.host}")
     private String redisHost;
@@ -22,6 +28,14 @@ public class RedisConfig {
 
     @Value("${spring.redis.password}")
     private String redisPassword;
+
+    @PostConstruct
+    public void init() {
+        log.info("=== REDIS CONFIG ===");
+        log.info("Host: {}", redisHost);
+        log.info("Port: {}", redisPort);
+        log.info("Password configurada: {}", redisPassword != null && !redisPassword.isEmpty());
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
