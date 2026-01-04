270a64f9f56657da6e3678a9831e9e1e60bdee70
diego <diegouf495@gmail.com>
2026-01-04 19:51:13 -0300

    feat: not null CreateAddressCommand

--- app/src/main/java/com/diego/list/customers/command/CreateAddressCommand.java
+++ app/src/main/java/com/diego/list/customers/command/CreateAddressCommand.java
@@ -1,5 +1,6 @@
 package com.diego.list.customers.command;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,25 +13,35 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAddressCommand {
+    @NotNull
     private String recipient;
 
+    @NotNull
     private Number zip_code;
 
+    @NotNull
     private String street_address;
 
+    @NotNull
     private Number street_number;
 
+    @NotNull
     private String neighborhood;
 
+    @NotNull
     private String city;
 
+    @NotNull
     private String state;
 
     private String complement;
 
+    @NotNull
     private String phone_number;
 
+    @NotNull
     private UUID user_id;
 
+    @NotNull
     private Boolean is_default;
 }
