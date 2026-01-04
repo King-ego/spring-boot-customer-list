73fd9abf9e602d7cc1a3186d671feb5293d78cb3
diego <diegouf495@gmail.com>
2026-01-04 19:51:45 -0300

    feat: Device Id Long to UUID

--- app/src/main/java/com/diego/list/customers/model/Device.java
+++ app/src/main/java/com/diego/list/customers/model/Device.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "devices")
@@ -14,8 +15,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Device {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
