f22959dc1a87de03a26dc013256c5d0f530aabf8
Diego <diegosantos4957@gmail.com>
2026-01-10 17:51:22 -0300

    feat: add UpdateAddressCommand class for address update functionality

---
+++ app/src/main/java/com/diego/list/customers/command/address/UpdateAddressCommand.java
@@ -0,0 +1,31 @@
+package com.diego.list.customers.command.address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateAddressCommand {
+    private String recipient;
+
+    private Number zip_code;
+
+    private String street_address;
+
+    private Number street_number;
+
+    private String neighborhood;
+
+    private String city;
+
+    private String state;
+
+    private String complement;
+
+    private String phone_number;
+
+    private Boolean is_default;
+}
