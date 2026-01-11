c5b1822572b4b6e57f9c73cb39778c29e9c03872
Diego <diegosantos4957@gmail.com>
2026-01-11 20:47:36 -0300

    fix: remove commented-out imports in SessionValidationFilter

--- app/src/main/java/com/diego/list/customers/filter/SessionValidationFilter.java
+++ app/src/main/java/com/diego/list/customers/filter/SessionValidationFilter.java
@@ -1,18 +1,5 @@
 package com.diego.list.customers.filter;
 
-/*import com.fasterxml.jackson.databind.ObjectMapper;
-import com.stripe.service.checkout.SessionService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;*/
-
 import com.diego.list.customers.model.Session;
 import com.diego.list.customers.security.SessionAuthentication;
 import com.diego.list.customers.services.SessionService;
@@ -21,9 +8,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletRequestWrapper;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
