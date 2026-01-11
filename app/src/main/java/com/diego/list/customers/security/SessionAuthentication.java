1e9d9af030394d3cd7e3592ed2837933868b0d6a
Diego <diegosantos4957@gmail.com>
2026-01-11 00:13:21 -0300

    feat: add SessionAuthentication class for handling session-based authentication

---
+++ app/src/main/java/com/diego/list/customers/security/SessionAuthentication.java
@@ -0,0 +1,32 @@
+package com.diego.list.customers.security;
+
+import com.diego.list.customers.model.Session;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+import java.util.Collection;
+
+
+public class SessionAuthentication extends AbstractAuthenticationToken{
+    private final Session session;
+
+    public SessionAuthentication(Session session, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.session = session;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return session.getSessionId();
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return session.getUserId();
+    }
+
+    public Session getSession() {
+        return session;
+    }
+}
