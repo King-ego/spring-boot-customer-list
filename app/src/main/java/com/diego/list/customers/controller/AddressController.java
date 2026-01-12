90d757a6751f608532e9e9d656182162eead19a6
Diego <diegosantos4957@gmail.com>
2026-01-12 20:45:09 -0300

    fix: update AddressController to return HTTP status for create and update operations

--- app/src/main/java/com/diego/list/customers/controller/AddressController.java
+++ app/src/main/java/com/diego/list/customers/controller/AddressController.java
@@ -3,6 +3,8 @@ package com.diego.list.customers.controller;
 import com.diego.list.customers.command.address.*;
 import com.diego.list.customers.services.AddressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -13,13 +15,15 @@ import java.util.UUID;
 public class AddressController {
     private final AddressService addressService;
 
-    @PostMapping("")
-    public void createAddress(@RequestBody CreateAddressCommand createAddressCommand) {
+    @PostMapping
+    public String createAddress(@RequestBody CreateAddressCommand createAddressCommand) {
         addressService.CreateAddress(createAddressCommand);
+        return ResponseEntity.status(HttpStatus.CREATED).toString();
     }
 
     @PatchMapping("/{addressId}")
-    public void updateAddress(@PathVariable UUID addressId, @RequestBody UpdateAddressCommand updateAddressCommand) {
+    public String updateAddress(@PathVariable UUID addressId, @RequestBody UpdateAddressCommand updateAddressCommand) {
         addressService.UpdateAddress(addressId, updateAddressCommand);
+        return ResponseEntity.status(HttpStatus.CREATED).toString();
     }
 }
