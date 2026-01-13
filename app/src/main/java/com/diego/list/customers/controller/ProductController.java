a8a652a0d6b9fbff4a45336c1aac36d9876b0915
Diego <diegosantos4957@gmail.com>
2026-01-12 22:52:39 -0300

    fix: update ProductController to change endpoint mapping for products

--- app/src/main/java/com/diego/list/customers/controller/ProductController.java
+++ app/src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/products")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
