ad7f4575ef778a06c2ace768b7e07e1d298c4424
Diego <diegosantos4957@gmail.com>
2026-01-14 09:07:26 -0300

    feat: add custom query method to AddressRepository for retrieving addresses by user ID

--- app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
@@ -2,8 +2,12 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AddressRepository extends JpaRepository<Address, UUID> {
+    @Query("SELECT a FROM Address a WHERE a.user.id = :userId")
+    List<Address> findByUserId(UUID userId);
 }
