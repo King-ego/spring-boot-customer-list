c64d5c7fcb9fb301a744bdf3c8f4405ad1bacb95
Diego <diegosantos4957@gmail.com>
2026-01-16 21:26:12 -0300

    feat: implement PaymentResultConsumer to handle payment result events

--- app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
@@ -1,4 +1,21 @@
 package com.diego.list.customers.fila.exchange.consumer;
 
+import com.diego.list.customers.fila.exchange.config.PaymentQueueConfig;
+import com.diego.list.customers.fila.exchange.event.PaymentProcessedEvent;
+import com.diego.list.customers.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class PaymentResultConsumer {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @RabbitListener(queues = PaymentQueueConfig.QUEUE_NAME)
+    public void receivePaymentResult(PaymentProcessedEvent event) {
+        log.info("Received PaymentResult Event: {}", event);
+    }
 }
