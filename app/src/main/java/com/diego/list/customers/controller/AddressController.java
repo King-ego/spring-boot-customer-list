cb6e218e0699e84bebde022447846c44f894a2eb
Diego <diegosantos4957@gmail.com>
2026-01-17 12:56:06 -0300

    fix: remove unused imports from AddressController.java

--- app/src/main/java/com/diego/list/customers/controller/AddressController.java
+++ app/src/main/java/com/diego/list/customers/controller/AddressController.java
@@ -4,8 +4,6 @@ import com.diego.list.customers.command.address.*;
 import com.diego.list.customers.model.Address;
 import com.diego.list.customers.services.AddressService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
