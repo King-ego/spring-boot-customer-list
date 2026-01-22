5f8a85f342ed3ec6ca61a0f3904b0e13bc49d355
Diego <diegosantos4957@gmail.com>
2026-01-22 09:36:44 -0300

    fix: update package structure and import statements in CreateAccountUseCase

--- app/src/main/java/com/diego/list/customers/application/usecase/account/CreateAccountUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/account/CreateAccountUseCase.java
@@ -1,7 +1,7 @@
-package com.diego.list.customers.application.usecases.account;
+package com.diego.list.customers.application.usecase.account;
 
-import com.diego.list.customers.command.createUser.CreateCustomerCommand;
-import com.diego.list.customers.command.createUser.CreateSellerCommand;
+import com.diego.list.customers.application.command.createUser.CreateCustomerCommand;
+import com.diego.list.customers.application.command.createUser.CreateSellerCommand;
 import com.diego.list.customers.model.Customer;
 import com.diego.list.customers.model.Seller;
 import com.diego.list.customers.repository.CustomerRepository;
