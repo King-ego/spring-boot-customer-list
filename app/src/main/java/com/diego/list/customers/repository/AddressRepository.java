678f78d12f6b10a312f57b0b4a069f338b979b58
Diego <diegosantos4957@gmail.com>
2026-01-22 08:49:05 -0300

    fix: add transactional support and modifying annotation to address update query

--- app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/AddressRepository.java
@@ -2,7 +2,9 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +12,8 @@ import java.util.UUID;
 public interface AddressRepository extends JpaRepository<Address, UUID> {
     @Query("SELECT a FROM Address a WHERE a.user.id = :userId")
     List<Address> findByUserId(UUID userId);
+    @Modifying
+    @Transactional
     @Query("UPDATE Address a SET a.isDefault = :isDefault WHERE a.id = :addressId")
     void updateParse(UUID addressId, boolean isDefault);
 }
