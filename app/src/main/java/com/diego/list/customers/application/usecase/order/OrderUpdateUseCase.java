5e31e873e4960c6734d7cef6193e7e1d243541ae
Diego <diegosantos4957@gmail.com>
2026-01-25 18:26:10 -0300

    feat: enhance updateOrderStatus method to handle missing orders and include paymentId and paymentDate

--- app/src/main/java/com/diego/list/customers/application/usecase/order/OrderUpdateUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/order/OrderUpdateUseCase.java
@@ -1,11 +1,13 @@
 package com.diego.list.customers.application.usecase.order;
 
+import com.diego.list.customers.model.Order;
 import com.diego.list.customers.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -15,6 +17,12 @@ public class OrderUpdateUseCase {
     private final OrderRepository orderRepository;
 
     public void updateOrderStatus(UUID orderId, String status, String paymentId, LocalDateTime paymentDate) {
-        // Method implementation goes here
+        Optional <Order> order = orderRepository.findById(orderId);
+
+        if (order.isEmpty()) {
+            throw new IllegalArgumentException("Order not found");
+
+        }
+        orderRepository.updateParse(orderId, status, paymentId, paymentDate);
     }
 }
