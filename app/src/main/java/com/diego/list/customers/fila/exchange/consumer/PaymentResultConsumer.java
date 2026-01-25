c2bfd878b486e6e907fc38dd4e8aa7a07276ef73
Diego <diegosantos4957@gmail.com>
2026-01-25 18:26:21 -0300

    feat: integrate OrderUpdateUseCase in PaymentResultConsumer for handling payment results

--- app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
@@ -1,21 +1,27 @@
 package com.diego.list.customers.fila.exchange.consumer;
 
+import com.diego.list.customers.application.usecase.order.OrderUpdateUseCase;
 import com.diego.list.customers.fila.exchange.config.PaymentQueueConfig;
 import com.diego.list.customers.fila.exchange.event.PaymentProcessedEvent;
-import com.diego.list.customers.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class PaymentResultConsumer {
-    @Autowired
-    private OrderRepository orderRepository;
+    private OrderUpdateUseCase orderUpdateUseCase;
 
     @RabbitListener(queues = PaymentQueueConfig.QUEUE_NAME)
     public void receivePaymentResult(PaymentProcessedEvent event) {
+
+        orderUpdateUseCase.updateOrderStatus(
+                event.getOrderId(),
+                event.getPaymentStatus(),
+                event.getPaymentId(),
+                event.getPaymentDate()
+        );
+
         log.info("Received PaymentResult Event: {}", event);
     }
 }
