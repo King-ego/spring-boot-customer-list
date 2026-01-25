950c459becf3792f01f5825dcff847ab2311c4d6
Diego <diegosantos4957@gmail.com>
2026-01-25 18:26:04 -0300

    feat: update updateParse method in OrderRepository to include paymentDate parameter

--- app/src/main/java/com/diego/list/customers/repository/OrderRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/OrderRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query("UPDATE Order o SET o.status = :status, o.paymentId = :paymentId WHERE o.id = :orderId")
-    public void updateParse(UUID orderId, String status, String paymentId);
+    @Query("UPDATE Order o SET o.status = :status, o.paymentId = :paymentId, o.paymentDate = :paymentDate WHERE o.id = :orderId")
+    public void updateParse(UUID orderId, String status, String paymentId, LocalDateTime paymentDate);
 }
