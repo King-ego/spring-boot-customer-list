58dabe8bd69b9192fa60d447959036024bf67868
Diego <diegosantos4957@gmail.com>
2026-01-26 09:57:37 -0300

    feat: update RabbitMQConsumer to handle PaymentProcessedEvent and log additional payment details

--- app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConsumer.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQConsumer.java
@@ -1,23 +1,28 @@
 package com.diego.list.customers.fila.exchange;
 
+import com.diego.list.customers.application.usecase.order.OrderUpdateUseCase;
 import com.diego.list.customers.fila.exchange.config.PaymentQueueConfig;
-import com.diego.list.customers.fila.exchange.event.OderCreateEvent;
+import com.diego.list.customers.fila.exchange.event.PaymentProcessedEvent;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class RabbitMQConsumer {
-
+    private final OrderUpdateUseCase orderUpdateUseCase;
     @RabbitListener(queues = PaymentQueueConfig.PAYMENT_RESULT_QUEUE)
-    private void receivePaymentResult(OderCreateEvent event) {
-        log.info("Processing order {} for customer {} with total: {}",
+    private void receivePaymentResult(PaymentProcessedEvent event) {
+        log.info("Processing order {} for customer {} with total: {} and status: {}",
                 event.getGroupId(),
                 event.getCustomerId(),
-                event.getOrderTotal());
+                event.getPaymentId(),
+                event.getStatus());
 
-        log.info("Order {} processed successfully", event.getGroupId());
+        log.info("Order {} processed successfully", event);
+
     }
 
 
