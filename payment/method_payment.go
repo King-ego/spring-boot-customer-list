2e044c82f904900385eb988a75cebdb391ea0fb9
Diego <diegosantos4957@gmail.com>
2026-01-26 09:36:54 -0300

    feat: add event structures and processing function for payment events

--- payment/method_payment.go
+++ payment/method_payment.go
@@ -1,1 +1,27 @@
 package payment
+
+type ProcessedEvent struct {
+	GroupID     string `json:"groupId"`
+	CustomerID  string `json:"customerId"`
+	Status      string `json:"status"`
+	PaymentId   string `json:"paymentId"`
+	PaymentDate string `json:"paymentDate"`
+}
+
+type OrderCreatedEvent struct {
+	GroupID     string  `json:"groupId"`
+	CustomerID  string  `json:"customerId"`
+	OrderStatus string  `json:"orderStatus"`
+	OrderTotal  float64 `json:"orderTotal"`
+}
+
+func Processed(event OrderCreatedEvent) (ProcessedEvent, error) {
+
+	return ProcessedEvent{
+		GroupID:     event.GroupID,
+		CustomerID:  event.CustomerID,
+		Status:      "APPROVED",
+		PaymentId:   "PAY-123456",
+		PaymentDate: "2024-01-01T12:00:00Z",
+	}, nil
+}
