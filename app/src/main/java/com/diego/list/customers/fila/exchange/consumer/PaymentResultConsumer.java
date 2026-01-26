ab3ceff1fa1bd26395ea8b551d54665bb8772303
Diego <diegosantos4957@gmail.com>
2026-01-26 00:28:47 -0300

    feat: enhance PaymentResultConsumer to handle exceptions during payment processing

--- app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/consumer/PaymentResultConsumer.java
@@ -3,25 +3,33 @@ package com.diego.list.customers.fila.exchange.consumer;
 import com.diego.list.customers.application.usecase.order.OrderUpdateUseCase;
 import com.diego.list.customers.fila.exchange.config.PaymentQueueConfig;
 import com.diego.list.customers.fila.exchange.event.PaymentProcessedEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PaymentResultConsumer {
-    private OrderUpdateUseCase orderUpdateUseCase;
+    private final OrderUpdateUseCase orderUpdateUseCase;
 
     @RabbitListener(queues = PaymentQueueConfig.QUEUE_NAME)
     public void receivePaymentResult(PaymentProcessedEvent event) {
+        try{
+            orderUpdateUseCase.updateOrderStatus(
+                    event.getOrderId(),
+                    event.getPaymentStatus(),
+                    event.getPaymentId(),
+                    event.getPaymentDate()
+            );
 
-        orderUpdateUseCase.updateOrderStatus(
-                event.getOrderId(),
-                event.getPaymentStatus(),
-                event.getPaymentId(),
-                event.getPaymentDate()
-        );
+            log.info("Received PaymentResult Event: {}", event);
+        } catch (Exception exc){
+            log.error("Failed to process payment for orderId: {}. Error: {}",
+                    event.getOrderId(), exc.getMessage(), exc);
+            throw exc;
+        }
 
-        log.info("Received PaymentResult Event: {}", event);
     }
 }
