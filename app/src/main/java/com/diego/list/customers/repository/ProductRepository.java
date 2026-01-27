890f0a1e739735471b543420081dd70d2c84f300
Diego <diegosantos4957@gmail.com>
2026-01-27 00:06:14 -0300

    feat: add method to update product quantity in stock in ProductRepository

--- app/src/main/java/com/diego/list/customers/repository/ProductRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,4 +17,8 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     @Query("SELECT p FROM Product p WHERE p.name LIKE :identifier% OR p.category LIKE :identifier% OR p.description LIKE :identifier%")
     List<Product> findByIdentity(String identifier);
+
+    @Modifying
+    @Query("UPDATE Product p SET p.quantityInStock = COALESCE(:quantityInStock, p.quantityInStock) WHERE p.id = :id")
+    void updatedParse(UUID id, int quantityInStock);
 }
