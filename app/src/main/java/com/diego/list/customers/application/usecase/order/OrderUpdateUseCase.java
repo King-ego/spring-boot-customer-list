75d2ad33f80736b088d358ab34c5054f64e85eb1
Diego <diegosantos4957@gmail.com>
2026-01-27 00:04:30 -0300

    feat: update OrderUpdateUseCase to handle order updates by groupId and improve error handling

--- app/src/main/java/com/diego/list/customers/application/usecase/order/OrderUpdateUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/order/OrderUpdateUseCase.java
@@ -1,28 +1,34 @@
 package com.diego.list.customers.application.usecase.order;
 
+import com.diego.list.customers.errors.CustomException;
 import com.diego.list.customers.model.Order;
 import com.diego.list.customers.repository.OrderRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 @Transactional
 public class OrderUpdateUseCase {
     private final OrderRepository orderRepository;
 
-    public void updateOrderStatus(UUID orderId, String status, String paymentId, LocalDateTime paymentDate) {
-        Optional <Order> order = orderRepository.findById(orderId);
+    public void updateOrderStatus(UUID groupId, String status, String paymentId, LocalDateTime paymentDate) {
+        List <Order> order = orderRepository.findAllGroupId(groupId);
 
         if (order.isEmpty()) {
-            throw new IllegalArgumentException("Order not found");
-
+            log.warn("No orders found for groupId: {}", groupId);
+            throw new CustomException("Orders not found for the provided groupId", HttpStatus.NOT_FOUND);
         }
-        orderRepository.updateParse(orderId, status, paymentId, paymentDate);
+
+        order.forEach(o -> orderRepository.updateParse(o.getGroupId(), status, paymentId, paymentDate));
+
     }
 }
