fd2579ee589e397fea8ca557f114f0b8bd3fff74
Diego <diegosantos4957@gmail.com>
2026-01-27 00:04:24 -0300

    feat: update OrderRepository to modify orders by groupId and add method to find orders by groupId

--- app/src/main/java/com/diego/list/customers/repository/OrderRepository.java
+++ app/src/main/java/com/diego/list/customers/repository/OrderRepository.java
@@ -2,14 +2,20 @@ package com.diego.list.customers.repository;
 
 import com.diego.list.customers.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query("UPDATE Order o SET o.status = :status, o.paymentId = :paymentId, o.paymentDate = :paymentDate WHERE o.id = :orderId")
-    public void updateParse(UUID orderId, String status, String paymentId, LocalDateTime paymentDate);
+    @Modifying
+    @Query("UPDATE Order o SET o.status = :status, o.paymentId = :paymentId, o.paymentDate = :paymentDate WHERE o.groupId = :groupId")
+    void updateParse(UUID groupId, String status, String paymentId, LocalDateTime paymentDate);
+
+    @Query("SELECT o FROM Order o WHERE o.groupId = :groupId")
+    List <Order> findAllGroupId(UUID groupId);
 }
