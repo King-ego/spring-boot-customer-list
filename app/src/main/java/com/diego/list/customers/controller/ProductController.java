c9793046c381a508e1715cbcca4413bb01ea322c
Diego <diegosantos4957@gmail.com>
2026-01-31 18:18:36 -0300

    chore: add blank line for improved readability in ProductController

--- app/src/main/java/com/diego/list/customers/controller/ProductController.java
+++ app/src/main/java/com/diego/list/customers/controller/ProductController.java
@@ -38,6 +38,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).toString();
     }
 
+
     @GetMapping
     public List<Product> index(@ModelAttribute FilterProductDto query){
         return productService.getAllProducts();
