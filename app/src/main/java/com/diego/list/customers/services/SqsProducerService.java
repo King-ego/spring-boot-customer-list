4777d9237c34f0f199d976a4928e7aab8583bd59
Diego <diegosantos4957@gmail.com>
2026-02-01 19:58:22 -0300

    chore: implement SqsProducerService for sending messages to SQS

--- app/src/main/java/com/diego/list/customers/services/SqsProducerService.java
+++ app/src/main/java/com/diego/list/customers/services/SqsProducerService.java
@@ -1,4 +1,22 @@
 package com.diego.list.customers.services;
 
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Service
 public class SqsProducerService {
+
+    private final SqsClient sqsClient;
+
+    public SqsProducerService(SqsClient sqsClient) {
+        this.sqsClient = sqsClient;
+    }
+
+    public void send(String queueUrl, String message) {
+        sqsClient.sendMessage(r -> r
+                .queueUrl(queueUrl)
+                .messageBody(message)
+        );
+    }
 }
+
