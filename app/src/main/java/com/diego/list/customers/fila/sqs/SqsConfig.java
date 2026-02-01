8ec22bbf377c75abf60300e2b5854d35624c61d2
Diego <diegosantos4957@gmail.com>
2026-02-01 19:58:38 -0300

    chore: add SqsClient bean configuration for AWS SQS

--- app/src/main/java/com/diego/list/customers/fila/sqs/SqsConfig.java
+++ app/src/main/java/com/diego/list/customers/fila/sqs/SqsConfig.java
@@ -1,7 +1,24 @@
 package com.diego.list.customers.fila.sqs;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
 public class SqsConfig {
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public SqsClient sqsClient() {
+        return SqsClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        EnvironmentVariableCredentialsProvider.create()
+                )
+                .build();
+    }
 }
