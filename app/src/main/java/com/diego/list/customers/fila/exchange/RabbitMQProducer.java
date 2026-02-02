7819ddc7b54f63a5dadd4d62d2168fb9a9c26f03
Diego <diegosantos4957@gmail.com>
2026-02-02 18:33:35 -0300

    feat: refactor RabbitMQProducer to use configuration properties for exchange and routing key

--- app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQProducer.java
+++ app/src/main/java/com/diego/list/customers/fila/exchange/RabbitMQProducer.java
@@ -1,28 +1,34 @@
 package com.diego.list.customers.fila.exchange;
 
-import com.diego.list.customers.fila.exchange.config.PaymentQueueConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class RabbitMQProducer {
     private final RabbitTemplate rabbitTemplate;
+    private final String orderExchange;
+    private final String orderRoutingKey;
 
-    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate,
+                            @Value("${rabbitmq.exchange.order.name}") String orderExchange,
+                            @Value("${rabbitmq.routing-key.order.created}") String orderRoutingKey) {
         this.rabbitTemplate = rabbitTemplate;
+        this.orderExchange = orderExchange;
+        this.orderRoutingKey = orderRoutingKey;
     }
 
     public void sendMessage(String message) {
-        rabbitTemplate.convertAndSend(PaymentQueueConfig.ORDER_EXCHANGE, PaymentQueueConfig.ORDER_ROUTING_KEY, message);
+        rabbitTemplate.convertAndSend(orderExchange, orderRoutingKey, message);
 
-        log.info("Sent message to exchange: {}", PaymentQueueConfig.ORDER_EXCHANGE);
+        log.info("Sent message to exchange: {}", orderExchange);
     }
 
     public void sendEvent(Object event) {
-        rabbitTemplate.convertAndSend(PaymentQueueConfig.ORDER_EXCHANGE, PaymentQueueConfig.ORDER_ROUTING_KEY, event);
+        rabbitTemplate.convertAndSend(orderExchange, orderRoutingKey, event);
 
-        log.info("Sent event to exchange: {}", PaymentQueueConfig.ORDER_EXCHANGE);
+        log.info("Sent event to exchange: {}", orderExchange);
     }
 }
