0e2b0b596aed9c0ca73283a51c386985152c4354
Diego <diegosantos4957@gmail.com>
2026-02-18 13:30:41 -0300

    feat: add validation methods to handle custom exceptions with HTTP status

--- app/src/main/java/com/diego/list/customers/utils/exceptions/ValidationUsersExceptions.java
+++ app/src/main/java/com/diego/list/customers/utils/exceptions/ValidationUsersExceptions.java
@@ -1,4 +1,16 @@
 package com.diego.list.customers.utils.exceptions;
 
+import com.diego.list.customers.errors.CustomException;
+import org.springframework.http.HttpStatus;
+
 public class ValidationUsersExceptions {
+    private static void validateCondition(boolean condition, String message, HttpStatus status) {
+        if (condition) {
+            throw new CustomException(message, status);
+        }
+    }
+
+    public static void validate(boolean condition, String message, HttpStatus status) {
+        validateCondition(condition, message, status);
+    }
 }
