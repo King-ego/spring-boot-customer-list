730e7a1a8e57ab5d555a732bb53ad3da3bf36078
Diego <diegosantos4957@gmail.com>
2026-02-18 14:10:21 -0300

    fix: rename ValidationUsersExceptions to ValidationExceptions for consistency

--- app/src/main/java/com/diego/list/customers/utils/exceptions/ValidationExceptions.java
+++ app/src/main/java/com/diego/list/customers/utils/exceptions/ValidationExceptions.java
@@ -3,7 +3,7 @@ package com.diego.list.customers.utils.exceptions;
 import com.diego.list.customers.errors.CustomException;
 import org.springframework.http.HttpStatus;
 
-public class ValidationUsersExceptions {
+public class ValidationExceptions {
     private static void validateCondition(boolean condition, String message, HttpStatus status) {
         if (condition) {
             throw new CustomException(message, status);
