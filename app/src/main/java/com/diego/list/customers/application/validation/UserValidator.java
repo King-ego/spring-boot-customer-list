bb48eb04b1e7c19154e52f03fd102b76e184514d
Diego <diegosantos4957@gmail.com>
2026-02-18 17:53:46 -0300

    refactor: remove unused imports in UserValidator for cleaner code

--- app/src/main/java/com/diego/list/customers/application/validation/UserValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/UserValidator.java
@@ -1,13 +1,8 @@
 package com.diego.list.customers.application.validation;
 
-import com.diego.list.customers.errors.CustomException;
-import com.diego.list.customers.model.User;
-import com.diego.list.customers.model.UserRole;
 import com.diego.list.customers.utils.exceptions.ValidationExceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 public class UserValidator {
