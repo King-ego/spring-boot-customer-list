3b5afa3df24253726399b1a8281ebe73e2f20575
Diego <diegosantos4957@gmail.com>
2026-02-21 01:32:50 -0300

    feat: add validateCustomerNotFound method to CustomerValidator for improved error handling

--- app/src/main/java/com/diego/list/customers/application/validation/CustomerValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/CustomerValidator.java
@@ -1,4 +1,14 @@
 package com.diego.list.customers.application.validation;
 
+import com.diego.list.customers.utils.exceptions.ValidationExceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerValidator {
+    public static void validateCustomerNotFound(Boolean condition) {
+        ValidationExceptions.validate(
+                condition, "Customer not found", HttpStatus.NOT_FOUND
+        );
+    }
 }
