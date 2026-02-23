31d8482fdc3b1ae3a8b4538fc475b2316886d3f4
Diego <diegosantos4957@gmail.com>
2026-02-23 14:51:04 -0300

    refactor: rename user validation methods to use exception handling

--- app/src/main/java/com/diego/list/customers/application/validation/UserValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/UserValidator.java
@@ -7,23 +7,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserValidator {
 
-    public static void validateUserExists(Boolean condition) {
+    public static void exceptionUserExists(Boolean condition) {
         ValidationExceptions
                 .validate(condition, "User is exist", HttpStatus.CONFLICT);
     }
 
-    public static void validateCustomerDetails(Boolean condition) {
+    public static void exceptionCustomerDetails(Boolean condition) {
         ValidationExceptions
                 .validate(condition, "Customer details are required", HttpStatus.BAD_REQUEST);
     }
 
-    public static void validateSellerDetails(Boolean condition) {
+    public static void exceptionSellerDetails(Boolean condition) {
         ValidationExceptions
                 .validate(condition, "Seller details are required", HttpStatus.BAD_REQUEST);
 
     }
 
-    public static void validateUserNotFound(Boolean condition) {
+    public static void exceptionUserNotFound(Boolean condition) {
         ValidationExceptions
                 .validate(condition, "User not found", HttpStatus.NOT_FOUND);
     }
