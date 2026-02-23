86a15a8d62dbac0ce4eba237898587c8d9a7fb2c
Diego <diegosantos4957@gmail.com>
2026-02-23 13:48:59 -0300

    feat: implement getClientIP method for retrieving client IP address

--- app/src/main/java/com/diego/list/customers/application/usecase/securityMonitor/GetClientIpUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/securityMonitor/GetClientIpUseCase.java
@@ -1,4 +1,15 @@
 package com.diego.list.customers.application.usecase.securityMonitor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GetClientIpUseCase {
+    public static String getClientIP(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader != null) {
+            return xfHeader.split(",")[0];
+        }
+        return request.getRemoteAddr();
+    }
 }
