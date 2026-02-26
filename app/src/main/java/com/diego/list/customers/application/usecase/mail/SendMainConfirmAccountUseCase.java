7958dda6aff88f5359ca0bc94a2ea542b96db785
Diego <diegosantos4957@gmail.com>
2026-02-26 19:03:32 -0300

    feat: inject SnsProducer and add @AllArgsConstructor to SendMainConfirmAccountUseCase

--- app/src/main/java/com/diego/list/customers/application/usecase/mail/SendMainConfirmAccountUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/mail/SendMainConfirmAccountUseCase.java
@@ -1,9 +1,13 @@
 package com.diego.list.customers.application.usecase.mail;
 
+import com.diego.list.customers.fila.aws.SnsProducer;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class SendMainConfirmAccountUseCase {
+    private final SnsProducer snsProducer;
     public void snsSendMessage(Object message) {
 
     }
