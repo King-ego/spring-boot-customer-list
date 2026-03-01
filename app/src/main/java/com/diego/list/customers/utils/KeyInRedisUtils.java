fe6d424ec66eaefb7581ba6c442571a88062fbab
Diego <diegosantos4957@gmail.com>
2026-03-01 01:32:41 -0300

    refactor: rename SaveKeyInRedisUtils to KeyInRedisUtils

--- app/src/main/java/com/diego/list/customers/utils/KeyInRedisUtils.java
+++ app/src/main/java/com/diego/list/customers/utils/KeyInRedisUtils.java
@@ -2,8 +2,6 @@ package com.diego.list.customers.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -12,7 +10,7 @@ import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
-public class SaveKeyInRedisUtils {
+public class KeyInRedisUtils {
     private final RedisTemplate<String, Object> redisTemplate;
     private final ObjectMapper objectMapper;
 
