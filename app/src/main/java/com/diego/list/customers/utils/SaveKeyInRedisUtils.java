a48f01c86e30be79a9e4e1353873ad5c90648301
Diego <diegosantos4957@gmail.com>
2026-03-01 01:31:17 -0300

    feat: implement punishKey method in SaveKeyInRedisUtils for key serialization and storage

--- app/src/main/java/com/diego/list/customers/utils/SaveKeyInRedisUtils.java
+++ app/src/main/java/com/diego/list/customers/utils/SaveKeyInRedisUtils.java
@@ -1,7 +1,31 @@
 package com.diego.list.customers.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
+@RequiredArgsConstructor
 public class SaveKeyInRedisUtils {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    public void punishKey(String key, Object value, Duration duration) {
+        try {
+            String serialized = objectMapper.writeValueAsString(value);
+            if (duration == null) {
+                redisTemplate.opsForValue().set(key, serialized);
+            } else {
+                redisTemplate.opsForValue().set(key, serialized, duration);
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Generate key error", e);
+        }
+    }
 }
