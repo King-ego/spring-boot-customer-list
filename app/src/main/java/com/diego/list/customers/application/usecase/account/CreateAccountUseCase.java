4ae95da6487b7c2ed53e7c32bd63ab0739be1c32
Diego <diegosantos4957@gmail.com>
2026-03-02 15:35:37 -0300

    feat: enhance createAccount method to support role-based user creation

--- app/src/main/java/com/diego/list/customers/application/usecase/account/CreateAccountUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/account/CreateAccountUseCase.java
@@ -2,14 +2,18 @@ package com.diego.list.customers.application.usecase.account;
 
 import com.diego.list.customers.application.command.users.CreateCustomerCommand;
 import com.diego.list.customers.application.command.users.CreateSellerCommand;
+import com.diego.list.customers.application.command.users.CreateUserCommand;
 import com.diego.list.customers.model.Customer;
 import com.diego.list.customers.model.Seller;
 import com.diego.list.customers.model.User;
+import com.diego.list.customers.model.UserRole;
 import com.diego.list.customers.repository.CustomerRepository;
 import com.diego.list.customers.repository.SellerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -18,7 +22,30 @@ public class CreateAccountUseCase {
     private final CustomerRepository customerRepository;
     private final SellerRepository sellerRepository;
 
-    public void createAccount(User user){
+    public void createAccount(CreateUserCommand command, User create_user) {
+        Map<UserRole, Runnable> validatedCreateRole = Map.of(
+                UserRole.CUSTOMER, () -> {
+                    CreateCustomerCommand customerCommand = CreateCustomerCommand.builder()
+                            .user(create_user)
+                            .document(command.getCustomerDetails().getDocument())
+                            .build();
+                    this.createRoleCustomer(customerCommand);
+                },
+                UserRole.SELLER, () -> {
+                    CreateSellerCommand sellerCommand = CreateSellerCommand.builder()
+                            .user(create_user)
+                            .storeName(command.getSellerDetails().getStoreName())
+                            .documentNumber(command.getSellerDetails().getDocumentNumber())
+                            .storeDescription(command.getSellerDetails().getStoreDescription())
+                            .build();
+                    this.createRoleSeller(sellerCommand);
+                }
+        );
+
+        Runnable action = validatedCreateRole.get(command.getRole());
+        if (action != null) {
+            action.run();
+        }
 
     }
 
