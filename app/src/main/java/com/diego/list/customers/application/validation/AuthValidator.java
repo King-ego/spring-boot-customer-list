1dbb9747c76448458b8892f41958bfeb4adcd6b6
Diego <diegosantos4957@gmail.com>
2026-03-03 16:05:34 -0300

    feat: add isRiskyIP method to AuthValidator for IP validation

--- app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
@@ -1,4 +1,11 @@
 package com.diego.list.customers.application.validation;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class AuthValidator {
+    public static Boolean isRiskyIP(String ip) {
+        return ip.startsWith("185.") || ip.startsWith("104.");
+    }
 }
