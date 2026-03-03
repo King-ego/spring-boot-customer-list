515867d1772f25cd796ae63271f3986dedcf26b6
Diego <diegosantos4957@gmail.com>
2026-03-03 16:07:51 -0300

    fix: remove unnecessary blank line in AuthValidator class

--- app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
@@ -3,7 +3,6 @@ package com.diego.list.customers.application.validation;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class AuthValidator {
     public static Boolean isRiskyIP(String ip) {
         return ip.startsWith("185.") || ip.startsWith("104.");
