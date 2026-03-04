0da49d545f623d1504681dc618bfc495a37b5db6
Diego <diegosantos4957@gmail.com>
2026-03-04 14:47:49 -0300

    feat: add isUnusualTime method to AuthValidator for time-based validation

--- app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
@@ -2,9 +2,18 @@ package com.diego.list.customers.application.validation;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class AuthValidator {
     public static Boolean isRiskyIP(String ip) {
         return ip.startsWith("185.") || ip.startsWith("104.");
+    }
+
+    public static Boolean isUnusualTime(){
+        int currentHour = LocalDateTime.now().getHour();
+        int startHour = 8;
+        int endHour = 20;
+        return currentHour < startHour || currentHour > endHour;
     }
 }
