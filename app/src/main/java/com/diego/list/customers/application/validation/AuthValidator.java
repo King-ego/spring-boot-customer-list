5899947e1bca2ddaaba1f6ed072032f9abad7ec2
Diego <diegosantos4957@gmail.com>
2026-03-04 14:51:58 -0300

    feat: modify isUnusualTime method in AuthValidator to accept User parameter

--- app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
+++ app/src/main/java/com/diego/list/customers/application/validation/AuthValidator.java
@@ -1,5 +1,6 @@
 package com.diego.list.customers.application.validation;
 
+import com.diego.list.customers.model.User;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,7 @@ public class AuthValidator {
         return ip.startsWith("185.") || ip.startsWith("104.");
     }
 
-    public static Boolean isUnusualTime(){
+    public static Boolean isUnusualTime(User user){
         int currentHour = LocalDateTime.now().getHour();
         int startHour = 8;
         int endHour = 20;
