ed088cead576c5fc45cf473bb7257b5587ff8401
Diego <diegosantos4957@gmail.com>
2026-03-05 10:03:14 -0300

    feat: implement execute method in CheckAndBlockAccountUseCase to count recent login failures

--- app/src/main/java/com/diego/list/customers/application/usecase/account/CheckAndBlockAccountUseCase.java
+++ app/src/main/java/com/diego/list/customers/application/usecase/account/CheckAndBlockAccountUseCase.java
@@ -1,4 +1,23 @@
 package com.diego.list.customers.application.usecase.account;
 
+import com.diego.list.customers.repository.SecurityLogRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class CheckAndBlockAccountUseCase {
+    private final SecurityLogRepository securityLogRepository;
+
+    public void execute(UUID userId, HttpServletRequest request) {
+        long failureCount = securityLogRepository.countRecentFailures(userId, LocalDateTime.now().minusMinutes(15));
+
+        if (failureCount >= 5) {
+
+        }
+    }
 }
