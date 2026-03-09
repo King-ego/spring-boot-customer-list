3ea7a73b79bf958f58e5bfed6d76b9298714a77b
Diego <diegosantos4957@gmail.com>
2026-03-09 15:23:51 -0300

    fix: add SpringExtension for JUnit 5 support in UserServicesTest.java

--- app/src/test/java/com/diego/list/customers/services/UserServicesTest.java
+++ app/src/test/java/com/diego/list/customers/services/UserServicesTest.java
@@ -1,4 +1,8 @@
 package com.diego.list.customers.services;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 public class UserServicesTest {
 }
