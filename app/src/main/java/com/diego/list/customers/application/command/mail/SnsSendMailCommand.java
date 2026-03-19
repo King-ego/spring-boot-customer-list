2999fd59c65861a92a866dca3c849eef33ccedde
Diego <diegosantos4957@gmail.com>
2026-03-19 16:50:35 -0300

    feat: add receiver field to SnsSendMailCommand for user-specific email handling

--- app/src/main/java/com/diego/list/customers/application/command/mail/SnsSendMailCommand.java
+++ app/src/main/java/com/diego/list/customers/application/command/mail/SnsSendMailCommand.java
@@ -1,5 +1,6 @@
 package com.diego.list.customers.application.command.mail;
 
+import com.diego.list.customers.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +14,5 @@ public class SnsSendMailCommand {
     private String email;
     private String name;
     private String message;
+    private User receiver;
 }
